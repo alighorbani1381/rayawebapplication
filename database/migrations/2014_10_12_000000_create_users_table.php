2ea03b19b8d92d0a67098e0ceaebef4bd2d07990
Ali <alighorbani20002@gmail.com>
2020-06-05 06:58:52 +0430

    update user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,11 +16,13 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('lastname');
+            $table->string('phone')->unique()->nullable();
+            $table->string('address');
+            $table->string('profile');
+            $table->enum('type', ['admin', 'contractor', 'taskmaster']);
+            $table->string('email')->unique()->nullable();
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
         });
     }
 
