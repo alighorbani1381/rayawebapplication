66c21e6c559d1711c9860b6a60f6fbdb7d0e700a
Ali <alighorbani20002@gmail.com>
2020-06-14 12:34:00 +0430

    set layout page

---
+++ resources/views/Admin/Layout/footer.blade.php
@@ -0,0 +1,36 @@
+<script data-cfasync="false" src="../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script>
+    var resizefunc = [];
+</script>
+
+<!-- jQuery  -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap-rtl.min.js"></script>
+<script src="assets/js/detect.js"></script>
+<script src="assets/js/fastclick.js"></script>
+<script src="assets/js/jquery.blockUI.js"></script>
+<script src="assets/js/waves.js"></script>
+<script src="assets/js/jquery.nicescroll.js"></script>
+<script src="assets/js/jquery.slimscroll.js"></script>
+<script src="assets/js/jquery.scrollTo.min.js"></script>
+
+<!-- KNOB JS -->
+<!--[if IE]>
+<script type="text/javascript" src="assets/plugins/jquery-knob/excanvas.js"></script>
+<![endif]-->
+<script src="assets/plugins/jquery-knob/jquery.knob.js"></script>
+
+<!--Morris Chart-->
+<script src="assets/plugins/morris/morris.min.js"></script>
+<script src="assets/plugins/raphael/raphael-min.js"></script>
+
+<!-- Dashboard init -->
+<script src="assets/pages/jquery.dashboard.js"></script>
+
+<!-- App js -->
+<script src="assets/js/jquery.core.js"></script>
+<script src="assets/js/jquery.app.js"></script>
+
+</body>
+
+<!-- Mirrored from webmasterlabs.ir/Mr-Admin/ by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 02 Oct 2018 20:33:36 GMT -->
+</html>
---
+++ resources/views/Admin/Layout/main.blade.php
@@ -0,0 +1,9 @@
+@include('Admin.Layout.header')
+
+@include('Admin.Layout.leftMenu')
+
+@include('Admin.Layout.content')
+
+@include('Admin.Layout.rightMenu')
+
+@include('Admin.Layout.footer')
