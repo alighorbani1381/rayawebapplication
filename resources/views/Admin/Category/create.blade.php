3a828293d8fc6361b68abb0722a6054285591591
Ali <alighorbani20002@gmail.com>
2020-06-15 09:31:16 +0430

    add create category view

---
+++ resources/views/Admin/Category/create.blade.php
@@ -0,0 +1,47 @@
+@extends('Admin.Layout.main')
+@section('title', 'افزودن خدمات جدید')
+@section('header', 'افزودن خدمات جدید')
+@section('content')
+<div class="row">
+    <div class="col-sm-10 col-lg-offset-1">
+        <div class="card-box">
+            <h4 class="header-title m-t-0 m-b-30">افزودن خدمت جدید</h4>
+            <div class="row">
+                <div class="col-lg-10">
+                    <form class="form-horizontal" role="form" action="{{ route('category.store') }}" method="post">
+                        @csrf
+                        <div class="form-group">
+                            <label class="col-md-2 control-label">عنوان خدمات</label>
+                            <div class="col-md-10">
+                                <input type="text" name="title" class="form-control" value="{{ old('title') }}" placeholder="عنوان خدمت را وارد کنید ...">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-md-2 control-label">توضیحات</label>
+                            <div class="col-md-10">
+                                <textarea class="form-control txt-custom" placeholder="توضیحات خدمت را وارد کنید ..." name="description" rows="5">{{ old('description') }}</textarea>
+                            </div>
+                        </div>
+
+
+                        <div class="form-group">
+                            <label class="col-md-2 control-label">زیر گروه</label>
+                            <div class="col-sm-10">
+                                <select class="form-control" name="child">
+                                    <option value="0" selected>سرگروه</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-success waves-effect waves-light submit-button">افزودن خدمت</button>
+                        </div>
+                    </form>
+                </div><!-- end col -->
+
+            </div><!-- end row -->
+        </div>
+    </div><!-- end col -->
+</div>
+@endsection
