19da5b4b81a9ae4df54e5c7f945bf5f8ad591884
Ali <alighorbani20002@gmail.com>
2020-06-15 07:17:58 +0430

    create user

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -1,14 +1,40 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
-use App\User;
+
 use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Support\Facades\Session;
+
+class UserRequest
+{
+
+    public static function userValidation(Request $request)
+    {
+
+        $fileds = [
+            'name' => 'required|max:20',
+            'lastname' => 'required|max:30',
+            'phone' => 'required|numeric|min:11',
+            'address' => 'required',
+            'username' => 'required|unique:App\User,username|regex:/^[a-zA-Z]+$/u|min:8',
+        ];
+
+        $messages = [
+            'username.regex' => 'نام کاربری تنها می تواند دارای حروف انگلیسی باشد.',
+            'phone.numeric' => 'فرمت شماره موبایل وارد شده نا معتبر است.',
+        ];
+        $request->validate($fileds, $messages);
+    }
+
+
+}
 
 class UserController extends AdminController
 {
 
-    public function index(){
-
+    public function index()
+    {
     }
 
     public function login(Request $request)
@@ -22,31 +48,40 @@ class UserController extends AdminController
         return view('Admin.User.create');
     }
 
-   
+
     public function store(Request $request)
     {
-        //
+        UserRequest::userValidation($request);
+        User::create([
+            'name' => $request->name,
+            'lastname' => $request->lastname,
+            'phone' => $request->phone,
+            'address' => $request->address,
+            'type' => $request->type,
+            'username' => $request->username,
+            'password' => 'raya-px724',
+        ]);
     }
 
-   
+
     public function show(User $user)
     {
         //
     }
 
-   
+
     public function edit(User $user)
     {
         //
     }
 
-   
+
     public function update(Request $request, User $user)
     {
         //
     }
 
-   
+
     public function destroy(User $user)
     {
         //
