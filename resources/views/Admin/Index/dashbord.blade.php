f4cddc8cd591fbf8bbc06f6a4fe5163d299b2fb4
Ali <alighorbani20002@gmail.com>
2020-06-16 19:15:05 +0430

    add dashbord view

---
+++ resources/views/Admin/Index/dashbord.blade.php
@@ -0,0 +1,16 @@
+@extends('Admin.Layout.main')
+@section('title', 'رایا مدیر')
+@section('header', 'پنل مدیریتی رایا مدیر')
+@section('content')
+<div class="row">
+    <div class="col-sm-10 col-lg-offset-1">
+        <div class="card-box">
+            <div class="row">
+                <h1>به پنل مدیریتی رایا ادمین خوش اومدین !</h1>
+                <p>توی این صفحه ما قراره یه سری پیش نمایش رو به مدیر نشون بدیم که خیلی راحت بتونه کار ها رو مدیریت کنه.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
