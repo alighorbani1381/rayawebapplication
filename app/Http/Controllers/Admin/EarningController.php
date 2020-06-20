231ea095b498d9dc6174c68821fdbed8ed376951
Ali <alighorbani20002@gmail.com>
2020-06-20 08:49:36 +0430

    add Earnigns Controller

---
+++ app/Http/Controllers/Admin/EarningController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Earning;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class EarningController extends Controller
+{
+
+    public function index()
+    {
+        //
+    }
+
+
+    public function create()
+    {
+        //
+    }
+
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+   
+    public function show(Earning $earning)
+    {
+        //
+    }
+
+    
+    public function edit(Earning $earning)
+    {
+        //
+    }
+
+  
+    public function update(Request $request, Earning $earning)
+    {
+        //
+    }
+
+  
+    public function destroy(Earning $earning)
+    {
+        //
+    }
+}
