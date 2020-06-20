e8b8313c18e4997b65fe9928fa35fd6696f03343
Ali <alighorbani20002@gmail.com>
2020-06-20 09:33:17 +0430

    waves and other js file un comented

--- resources/views/Admin/Layout/footer.blade.php
+++ resources/views/Admin/Layout/footer.blade.php
@@ -1,13 +1,16 @@
-{{-- <script data-cfasync="false" src="../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script>
+<script data-cfasync="false" src="../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script>
     var resizefunc = [];
-</script> --}}
+</script> 
 
 <!-- jQuery  -->
 <script src="{{ asset('admin/js/bootstrap-rtl.min.js') }}"></script>
-{{-- <script src="{{ asset('admin/js/detect.js') }}"></script>
 <script src="{{ asset('admin/js/fastclick.js') }} "></script>
+
+{{-- <script src="{{ asset('admin/js/detect.js') }}"></script>
 <script src="{{ asset('admin/js/jquery.blockUI.js') }}"></script>
+ --}}
 <script src="{{ asset('admin/js/waves.js') }}"></script>
+{{-- 
 <script src="{{ asset('admin/js/jquery.nicescroll.js') }}"></script>
 <script src="{{ asset('admin/js/jquery.slimscroll.js') }}"></script>
 <script src="{{ asset('admin/js/jquery.scrollTo.min.js') }}"></script> --}}
@@ -17,10 +20,11 @@
 <script src="{{ asset('admin/plugins/jquery-knob/jquery.knob.js') }}"></script>
 
 <!--Morris Chart-->
-{{--
+
      <script src="{{ asset('admin/plugins/morris/morris.min.js') }}"></script>
-     <script src="{{ asset('admin/plugins/raphael/raphael-min.js') }}"></script> 
---}}
+     
+     {{-- <script src="{{ asset('admin/plugins/raphael/raphael-min.js') }}"></script>  --}}
+
 
 <!-- Dashboard init -->
 <script src="{{ asset('admin/pages/jquery.dashboard.js') }}"></script>
