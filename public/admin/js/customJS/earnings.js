f33cab04ff0b94f1f1ab4f150b8b18278cbd06ac
Ali <alighorbani20002@gmail.com>
2020-06-20 10:45:38 +0430

    Delete Earning box and show warning message

--- public/admin/js/customJS/earnings.js
+++ public/admin/js/customJS/earnings.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
         var $number = 0;
         $('.earning-holder').each(function () {
             var title = "name[" + $number + "]";
-            var money ="received_money[" + $number + "]";
+            var money = "received_money[" + $number + "]";
             var description = "description[" + $number + "]";
             var status = "status[" + $number + "]";
 
@@ -14,14 +14,38 @@ $(document).ready(function () {
             $(this).find('.earning-moeny').attr('name', money);
             $(this).find('.earning-description').attr('name', description);
             $(this).find('.earning-status').attr('name', status);
-            $number+=1;
+            $number += 1;
         });
 
     }
 
+    function mustExistOneBox(){
+        setTimeout(function () {
+            Swal.fire({
+                icon: 'warning',
+                title: "داداش داری اشتباه میزنی !",
+                text: " اینجا باید حداقل یه فیلد وجود داشته باشه" + "😐😑",
+                confirmButtonText: "حله",
+            });
+        }, 250);
+    }
+
+    // Add Earning Button Click
     $('#add-earning-box').on('click', function () {
         var box = $('.earning-holder').first().clone(true)
-        $('#main-holders').append(box);
+        $('#main-holders').hide().append(box).fadeIn();
+        refreshBoxNames();
+    });
+
+    // Delete Box
+    $('.delete-box').on('click', function () {
+        var boxCount = $('.earning-holder').length;
+        if (boxCount == 1){
+            mustExistOneBox();
+            return false;
+        }
+        $(this).parents('.earning-holder').remove();
+        $('#main-holders').hide().fadeIn();
         refreshBoxNames();
     });
 
