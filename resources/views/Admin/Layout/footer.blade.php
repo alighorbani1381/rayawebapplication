add5e07482744b1a44577a8355dcb16c9bc4692f
Ali <alighorbani20002@gmail.com>
2020-06-20 11:04:59 +0430

    comment waves js file just for distrube

--- resources/views/Admin/Layout/footer.blade.php
+++ resources/views/Admin/Layout/footer.blade.php
@@ -9,7 +9,7 @@
 {{-- <script src="{{ asset('admin/js/detect.js') }}"></script>
 <script src="{{ asset('admin/js/jquery.blockUI.js') }}"></script>
  --}}
-<script src="{{ asset('admin/js/waves.js') }}"></script>
+{{-- <script src="{{ asset('admin/js/waves.js') }}"></script> --}}
 {{-- 
 <script src="{{ asset('admin/js/jquery.nicescroll.js') }}"></script>
 <script src="{{ asset('admin/js/jquery.slimscroll.js') }}"></script>
@@ -21,7 +21,7 @@
 
 <!--Morris Chart-->
 
-     <script src="{{ asset('admin/plugins/morris/morris.min.js') }}"></script>
+     {{-- <script src="{{ asset('admin/plugins/morris/morris.min.js') }}"></script> --}}
      
      {{-- <script src="{{ asset('admin/plugins/raphael/raphael-min.js') }}"></script>  --}}
 
