191620330f2bf0aa506c4efe9c92221c04f74468
Ali <alighorbani20002@gmail.com>
2020-06-20 08:43:04 +0430

    Add and Execute Earning Migration

---
+++ database/migrations/2020_06_20_034736_create_earnings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEarningsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('earnings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('generator')->unsigned();;
+            $table->integer('project_id')->unsigned();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->integer('received_money');
+            $table->enum('status', ['paid', 'unpaid'])->default('unpaid');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('earnings');
+    }
+}
