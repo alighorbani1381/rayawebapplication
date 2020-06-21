b021d085e636c623081694372ca5791705a93054
Ali <alighorbani20002@gmail.com>
2020-06-21 11:59:58 +0430

    add index cost static file

---
+++ resources/views/Admin/CostStatic/index.blade.php
@@ -0,0 +1,73 @@
+@extends('Admin.Layout.main')
+@section('title', 'لیست هزینه ها ثابت شما')
+@section('header', 'لیست هزینه های ثابت شما')
+@push('js')
+<script src="{{ asset('admin/js/customJS/categories.js') }} "></script>
+@endpush
+@section('content')
+<div class="row">
+    <div class="col-sm-12">
+        <div class="card-box table-responsive">
+
+            <h4 class="header-title m-t-0 m-b-30 inb">
+                لیست هزینه های ثابت شما
+            </h4>
+
+            <a href="{{ route('static.create') }}"
+                class="cbfl btn btn-info btn-bordred waves-effect waves-dark m-b-5"> <i class="fa fa-plus-circle"></i>
+                <span>افزودن جدید </span> </a>
+
+
+            <table id="datatable" class="table table-striped table-bordered">
+                <thead>
+                    <tr>
+                        <th>ردیف</th>
+                        <th>عنوان هزینه</th>
+                        <th>توضیحات (خلاصه)</th>
+                        <th>سرگروه</th>
+                        <th class="tac">ویرایش</th>
+                        <th class="tac">حذف</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    @foreach ($costStatics as $row => $cost)
+                    <tr>
+                        <td><?= $row  + 1 ?></td>
+                        <td class="categoryName">{{ $cost->title }}</td>
+                        <td>{{ $cost->sub_desc }}</td>
+                        <td>{{ $cost->main_group }}</td>
+                        <td class="tac">
+                            <a href="{{ route('static.edit', $cost->id) }}"
+                                class="btn btn-icon waves-effect waves-light btn-info m-b-5"> <i
+                                    class="fa fa-pencil"></i> </a>
+                        </td>
+                        <td class="tac">
+                            <form method="post" action="{{ route('static.destroy', $cost->id) }}">
+                                @csrf
+                                @method('DELETE')
+                                <button type="button" class="delete-button btn btn-icon waves-effect waves-light btn-danger m-b-5"> <i
+                                        class="fa fa-remove"></i> </button>
+                            </form>
+                        </td>
+                    </tr>
+                    @endforeach
+
+                </tbody>
+            </table>
+        </div>
+    </div><!-- end col -->
+</div>
+@if(session()->has('DeleteCategoryFail'))
+    <script>maxMbox("حذف این مورد با شکست مواجه شد!", "این خدمت دارای زیر گروه است و نمی توان آن را حذف کرد", "error", "آها",350 );</script>
+@endif
+
+
+@if(session()->has('DeleteCategory'))
+    <script>minMbox('خدمت مورد نظر با موفقیت حذف شد.', 350);</script>
+@endif
+
+@if(session()->has('CategoryUpdate'))
+    <script>minMbox('خدمت مورد نظر با موفقیت ویرایش شد.', 350);</script>
+@endif
+@endsection
