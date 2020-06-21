f5019193d8e9ad386c7e7e75a3c17ff12e4e5080
Ali <alighorbani20002@gmail.com>
2020-06-21 11:49:12 +0430

    create view for cost static in controller

--- app/Http/Controllers/Admin/CoststaticController.php
+++ app/Http/Controllers/Admin/CoststaticController.php
@@ -8,77 +8,45 @@ use Illuminate\Http\Request;
 
 class CoststaticController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
     public function index()
     {
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
     public function create()
     {
-        //
+        $mainCategories = CostStatic::where('child', '0')->get();
+        return view('Admin.CostStatic.create', compact('mainCategories'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+   
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\CostStatic  $costStatic
-     * @return \Illuminate\Http\Response
-     */
+    
     public function show(CostStatic $costStatic)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\CostStatic  $costStatic
-     * @return \Illuminate\Http\Response
-     */
+   
     public function edit(CostStatic $costStatic)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\CostStatic  $costStatic
-     * @return \Illuminate\Http\Response
-     */
+    
+     
     public function update(Request $request, CostStatic $costStatic)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\CostStatic  $costStatic
-     * @return \Illuminate\Http\Response
-     */
+   
     public function destroy(CostStatic $costStatic)
     {
         //
