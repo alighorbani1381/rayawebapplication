e8a9f34b07bec34fb3f81f4e72468237ab4a96a8
Ali <alighorbani20002@gmail.com>
2020-06-21 15:00:37 +0430

    delete all static costs and sub static costs

--- app/Http/Controllers/Admin/CoststaticController.php
+++ app/Http/Controllers/Admin/CoststaticController.php
@@ -5,24 +5,25 @@ namespace App\Http\Controllers\Admin;
 use App\CostStatic;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CoststaticController extends Controller
 {
-    
+
     public function index()
     {
         $staticCosts = CostStatic::orderBy('child')->paginate(15);
         return view('Admin.CostStatic.index', compact('staticCosts'));
     }
 
-    
+
     public function create()
     {
         $mainCategories = CostStatic::where('child', '0')->get();
         return view('Admin.CostStatic.create', compact('mainCategories'));
     }
 
-   
+
     public function store(Request $request)
     {
         $request->validate(['title' => 'required', 'child' => 'required']);
@@ -30,13 +31,13 @@ class CoststaticController extends Controller
         return redirect()->route('static.index');
     }
 
-    
+
     public function show(CostStatic $costStatic)
     {
         return redirect()->route('static.index');
     }
 
-   
+
     public function edit($costStatic)
     {
         CostStatic::findOrFail($costStatic);
@@ -45,8 +46,8 @@ class CoststaticController extends Controller
         return view('Admin.CostStatic.edit', compact('costStatic', 'mainCategories'));
     }
 
-    
-     
+
+
     public function update(Request $request, $costStatic)
     {
         $costStatic = CostStatic::findOrFail($costStatic);
@@ -55,12 +56,27 @@ class CoststaticController extends Controller
         return redirect()->route('static.index');
     }
 
-   
+
     public function destroy($costStatic)
     {
         $costStatic = CostStatic::findOrFail($costStatic);
-        session()->flash('DeleteCostStatic');
-        $costStatic->delete();
+        if ($costStatic->child == 0) {
+            $subStatics = CostStatic::where('child', $costStatic->id)->select('id')->get();
+            foreach ($subStatics as $subStatic) {
+                DB::table('cost_statics')
+                    ->where('cost_statics.id', $subStatic->id)
+                    ->delete();
+            }
+            $costStatic->delete();
+
+            session()->flash('DeleteCostStaticAllMember');
+        } else {
+
+            $costStatic->delete();
+            session()->flash('DeleteCostStatic');
+        }
+
+
         return back();
     }
 }
