041b53e8e30bd404d94b2544646a71de320916f9
Ali <alighorbani20002@gmail.com>
2020-06-21 11:36:24 +0430

    add cost static controller

---
+++ app/Http/Controllers/Admin/CoststaticController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\CostStatic;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CoststaticController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\CostStatic  $costStatic
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CostStatic $costStatic)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\CostStatic  $costStatic
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(CostStatic $costStatic)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\CostStatic  $costStatic
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, CostStatic $costStatic)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\CostStatic  $costStatic
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CostStatic $costStatic)
+    {
+        //
+    }
+}
