b7593713e13f3267b5f9ffbc9a3c920deb91af1e
Ali <alighorbani20002@gmail.com>
2020-06-21 12:08:03 +0430

    add mutators into cost static model

--- app/CostStatic.php
+++ app/CostStatic.php
@@ -7,4 +7,31 @@ use Illuminate\Database\Eloquent\Model;
 class CostStatic extends Model
 {
     public $guarded = [];
+
+    public function getSubDescAttribute()
+    {
+        return mb_substr($this->description, 0, 50) . " ...";
+    }
+        
+    public function getMainGroupAttribute()
+    {
+        if ($this->child != 0){
+            $mainCategory = CostStatic::where('id', $this->child)->first();
+            $mainCategory = $mainCategory->title;
+        }
+        else
+            $mainCategory = "ندارد";
+
+            return $mainCategory;
+    }
+
+    public function getSubCatsAttribute()
+    {
+        if ($this->child == 0)
+            $subCategory = CostStatic::where('child', $this->id)->get();        
+        else
+            $subCategory = [];
+
+            return $subCategory;
+    }
 }
