851927f7845ab74e824a2bb9b70aab0a86ef4387
Ali <alighorbani20002@gmail.com>
2020-06-21 11:39:51 +0430

    change icon for cost

--- resources/views/Admin/Layout/leftMenu.blade.php
+++ resources/views/Admin/Layout/leftMenu.blade.php
@@ -36,7 +36,7 @@
 
 
                 <li class="has_sub">
-                    <a href="javascript:void(0);" class="waves-effect"><i class="fa fa-money"></i> <span> مدیریت هزینه ها</span> <span class="menu-arrow  fa-angle-left"></span></a>
+                    <a href="javascript:void(0);" class="waves-effect"><i class="fa fa-dollar"></i> <span> مدیریت هزینه ها</span> <span class="menu-arrow  fa-angle-left"></span></a>
                     <ul class="list-unstyled" style="display: none;">
                         <li><a href="{{ route('static.index') }}">لیست هزینه های ثابت</a></li>
                         <li><a href="{{ route('static.create') }}">ایجاد هزینه ثابت</a></li>
