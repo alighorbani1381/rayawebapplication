559a6b145fd6b8ceacfd4ea6eeeb5132e4dfc465
Ali <alighorbani20002@gmail.com>
2020-06-22 09:00:16 +0430

    add CostRepository class to create store method

--- app/Http/Controllers/Admin/CostController.php
+++ app/Http/Controllers/Admin/CostController.php
@@ -7,10 +7,33 @@ use App\CostStatic;
 use App\Http\Controllers\Controller;
 use App\Project;
 use Illuminate\Http\Request;
+use PhpParser\Builder\Class_;
 
+class CostRepository
+{
+
+    public function projectStore($request)
+    {
+        return $pay =  isset($request->contractor_pay) ? $this->projectContractorPay($request) : $this->projectStorePay($request);
+    }
+
+    public function projectStorePay($request)
+    {
+        return 'without contract pay';
+    }
+
+    public function projectContractorPay($request)
+    {
+        return 'contract pay';
+    }
+}
 class CostController extends Controller
 {
-   
+
+    public function __construct()
+    {
+        $this->repo = new CostRepository();
+    }
 
     public function index()
     {
@@ -26,7 +49,11 @@ class CostController extends Controller
 
     public function store(Request $request)
     {
-        //
+        $request->validate(['storeType' => 'required']);
+        $type = $request->get('storeType');
+
+        if ($type == 'project')
+            return $this->repo->projectStore($request);
     }
 
     public function show(Cost $cost)
