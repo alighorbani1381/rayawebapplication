bef8222513801931887527465bebcca738517d52
Ali <alighorbani20002@gmail.com>
2020-06-22 14:18:41 +0430

    add sub description mutator in the model

--- app/Cost.php
+++ app/Cost.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Cost extends Model
 {
     protected $guarded = [];
+
+    public function getSubDescAttribute()
+    {
+        return mb_substr($this->description, 0, 50) . " ...";
+    }
 }
