8ac1ebce0eaf839f9165542266928ced291ec821
Ali <alighorbani20002@gmail.com>
2020-06-22 13:23:11 +0430

    add form for extenal costs

--- resources/views/Admin/Cost/create.blade.php
+++ resources/views/Admin/Cost/create.blade.php
@@ -114,7 +114,7 @@
 
 {{-- Modals Start --}}
 
-<!-- Modal Pay User Start End -->
+<!-- Modal Pay Projet & User Start End -->
 <div class="row">
     <!-- Modal Pay User Cost Start -->
     <div id="pay-user-modal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
@@ -250,13 +250,13 @@
                     <div class="modal-footer">
                         <button type="button" class="btn btn-default waves-effect" data-dismiss="modal">بستن</button>
                         <button type="reset" class="btn btn-warning waves-effect">پاک کردن</button>
-                        <button type="submit" class="btn btn-success waves-effect waves-light">ثبت هزینه</button>
+                        <button type="submit" class="btn btn-success waves-effect waves-light">ثبت هزینه برای پروژه</button>
                     </div>
             </form>
         </div>
     </div>
 </div>
-<!-- Modal Pay User Cost End -->
+<!-- Modal Pay Project & User Cost End -->
 
 
 
@@ -264,69 +264,110 @@
 <div id="pay-external-modal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
     aria-hidden="true" style="display: none;">
     <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                <h4 class="modal-title">پرداخت هزینه های جانبی</h4>
-            </div>
-            <div class="modal-body">
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label for="field-1" class="control-label">نام</label>
-                            <input type="text" class="form-control" id="field-1" placeholder="علی">
+        <form action="{{ route('costs.store') }}" method="post">
+            @csrf
+            <input type="hidden" name="storeType" value="external">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                    <h4 class="modal-title">ثبت هزینه های جانبی</h4>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label for="field-3" class="control-label">عنوان هزینه</label>
+                                <input type="text" class="form-control" name="title" id="field-3"
+                                    placeholder="عنوان هزینه را وارد کنید ..."
+                                    value="@if(session()->has('ExtenalStore')){{ old('title') }}@endif">
+                            </div>
                         </div>
                     </div>
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label for="field-2" class="control-label">نام خانوادگی</label>
-                            <input type="text" class="form-control" id="field-2" placeholder="یدالهی">
+
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label for="field-3" class="control-label">میزان هزینه</label>
+                                <input type="number" class="form-control" name="money_paid" id="field-3"
+                                    placeholder="میزان هزینه را وارد کنید ..."
+                                    value="@if(session()->has('ExtenalStore')){{ old('money_paid') }}@endif">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group no-margin">
+                                <label for="field-7" class="control-label">توضیحات هزینه</label>
+                                <textarea class="form-control autogrow" id="field-7" name="description"
+                                    placeholder="توضیحات هزینه را وارد کنید ..."
+                                    style="overflow: hidden; word-wrap: break-word; resize: horizontal; height: 104px;">@if(session()->has('ExtenalStore')){{ old('description') }}@endif</textarea>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group no-margin">
+                                <label for="field-7" class="control-label">نوع هزینه</label>
+                                <select class="form-control" name="type">
+                                    <optgroup label="پیش فرض">
+                                        <option value="0">ندارد</option>
+                                    </optgroup>
+                                    @php $count = 0; @endphp
+                                    @foreach($types as $type)
+                                    @if($type->sub_cats->count() != 0)
+                                    <optgroup label="{{ $type->title }} ">
+                                        @foreach($type->sub_cats as $subCat)
+                                        <option value="{{ $subCat->id }}">{{ $subCat->title }} </option>
+                                        @endforeach
+                                    </optgroup>
+                                    @php $count += 1; @endphp
+                                    @endif
+                                    @endforeach
+
+                                    @if($count == 0)
+                                    <option disabled value="null">موردی برای نمایش وجود ندارد</option>
+                                    @endif
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="row">
+                        <div class="col-md-12" style="margin: 11px -8px;">
+                            <div class="form-group">
+                                <label class="col-md-2 control-label">وضعیت</label>
+                                <div class="pretty p-icon p-round p-pulse">
+                                    <input class="earning-paid" type="radio" name="status" checked value="paid">
+
+                                    <div class="state p-success">
+                                        <label>پرداخت شده</label> &nbsp; &nbsp; &nbsp; &nbsp;
+                                        <i class="icon mdi mdi-check"></i>
+                                    </div>
+                                </div>
+
+
+                                <div class="pretty p-icon p-round p-pulse">
+                                    <input class="earning-unpaid" type="radio" name="status" value="unpaid">
+
+                                    <div class="state p-danger">
+                                        <label>پرداخت نشده</label> &nbsp; &nbsp; &nbsp; &nbsp;
+                                        <i class="icon mdi mdi-check"></i>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="form-group">
-                            <label for="field-3" class="control-label">آدرس</label>
-                            <input type="text" class="form-control" id="field-3" placeholder="آدرس">
-                        </div>
-                    </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default waves-effect" data-dismiss="modal">بستن</button>
+                    <button type="reset" class="btn btn-warning waves-effect">پاک کردن</button>
+                    <button type="submit" class="btn btn-success waves-effect waves-light">ثبت هزینه جانبی</button>
                 </div>
-                <div class="row">
-                    <div class="col-md-4">
-                        <div class="form-group">
-                            <label for="field-4" class="control-label">شهر</label>
-                            <input type="text" class="form-control" id="field-4" placeholder="بابل">
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="form-group">
-                            <label for="field-5" class="control-label">کشور</label>
-                            <input type="text" class="form-control" id="field-5" placeholder="ایران">
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="form-group">
-                            <label for="field-6" class="control-label">کد پستی</label>
-                            <input type="text" class="form-control" id="field-6" placeholder="123456">
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="form-group no-margin">
-                            <label for="field-7" class="control-label">اطلاعات شخصی</label>
-                            <textarea class="form-control autogrow" id="field-7" placeholder="هر چیزی راجب به خودتان"
-                                style="overflow: hidden; word-wrap: break-word; resize: horizontal; height: 104px;"></textarea>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default waves-effect" data-dismiss="modal">بستن</button>
-                <button type="button" class="btn btn-info waves-effect waves-light">ذخیره</button>
-            </div>
-        </div>
+        </form>
     </div>
 </div>
 <!-- Modal Pay External Costs End -->
