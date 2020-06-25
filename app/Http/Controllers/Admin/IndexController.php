3a8e7ddaa1f99eb31573a977e4605b0e738f4d0a
Ali <alighorbani20002@gmail.com>
2020-06-25 12:24:08 +0430

    use project repository in the index controller

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Repository\ProjectRepository;
+
 class IndexController extends AdminController
 {
+
+    public function __construct()
+    {
+        $this->project = resolve(ProjectRepository::class);
+    }
+
+    
     public function index(){
+        $projects = $this->project->getStatisticProject();
         return view('Admin.Index.dashbord');
     }
 }
