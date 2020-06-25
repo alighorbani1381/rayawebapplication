6e41eab6907bd0ee3a7bfaa8bd9381e9dc3045c7
Ali <alighorbani20002@gmail.com>
2020-06-25 11:58:14 +0430

    add project widget

--- resources/views/Admin/Index/dashbord.blade.php
+++ resources/views/Admin/Index/dashbord.blade.php
@@ -3,14 +3,36 @@
 @section('header', 'پنل مدیریتی رایا مدیر')
 @section('content')
 <div class="row">
-    <div class="col-sm-10 col-lg-offset-1">
+
+    <div class="col-lg-3 col-md-6">
         <div class="card-box">
-            <div class="row">
-                <h1>به پنل مدیریتی رایا ادمین خوش اومدین !</h1>
-                <p>توی این صفحه ما قراره یه سری پیش نمایش رو به مدیر نشون بدیم که خیلی راحت بتونه کار ها رو مدیریت کنه.</p>
+            <div class="dropdown pull-right">
+                <a href="#" class="dropdown-toggle card-drop" data-toggle="dropdown" aria-expanded="false">
+                    <i class="zmdi zmdi-more-vert"></i>
+                </a>
+                <ul class="dropdown-menu" role="menu">
+                    <li><a href="#">فعال</a></li>
+                    <li><a href="#">متن اول</a></li>
+                    <li><a href="#">متن دوم</a></li>
+                    <li class="divider"></li>
+                    <li><a href="#">متن پاورقی</a></li>
+                </ul>
             </div>
+            <h4 class="header-title m-t-0 m-b-30">پروژه ها</h4>
+
+            <p class="font-600 m-b-5">سایت املاک <span class="text-primary pull-right">80%</span></p>
+            <div class="progress progress-bar-primary-alt progress-sm m-b-20">
+                <div class="progress-bar progress-bar-primary progress-animated wow animated animated"
+                    role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"
+                    style="width: 80%; visibility: visible; animation-name: animationProgress;">
+                </div>
+            </div>
+
+
+
         </div>
     </div>
+
 </div>
 
 @endsection
