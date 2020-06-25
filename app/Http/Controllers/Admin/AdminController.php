1a910322d2ab0330b80816fd0b9d9ace467ae7dc
Ali <alighorbani20002@gmail.com>
2020-06-25 11:53:08 +0430

    using admin to login

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+    public function __construct()
+    {
+        auth()->loginUsingId(5);
+    }
 }
