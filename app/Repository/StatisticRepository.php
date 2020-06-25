348723f077ac6160fb06809de682f336f2ba6659
Ali <alighorbani20002@gmail.com>
2020-06-25 13:31:30 +0430

    add getCountUsers method

--- app/Repository/StatisticRepository.php
+++ app/Repository/StatisticRepository.php
@@ -2,6 +2,7 @@
 namespace App\Repository;
 
 use App\Project;
+use App\User;
 
 class StatisticRepository{
 
@@ -13,6 +14,11 @@ class StatisticRepository{
     public function getCountActiveProjects()
     {
         return Project::where('status', '!=', 'finished')->count();
+    }
+
+    public function getCountUsers()
+    {
+        return User::count() - 1;
     }
 
 }
