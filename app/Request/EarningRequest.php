a350a31ae091407d4e5d14c0f86e0ebd0b5357f8
Ali <alighorbani20002@gmail.com>
2020-06-26 10:52:50 +0430

    add earning request class

---
+++ app/Request/EarningRequest.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Request;
+
+class EarningRequest
+{
+
+    public static function storeValidate($request)
+    {
+        $request->validate([
+            'title.*' => 'required',
+            'received_money.*' => 'required|numeric|min:1',
+            'status.*' => 'required',
+        ]);
+    }
+}
