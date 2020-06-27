e5eb709f8105de72ed056bee1eda7083a4380578
Ali <alighorbani20002@gmail.com>
2020-06-27 12:43:11 +0430

    add ongoing view to contractor panel

---
+++ resources/views/Contractor/Project/ongoing.blade.php
@@ -0,0 +1,77 @@
+@extends('Contractor.Layout.main')
+@section('title', 'لیست پروژه های فعال')
+@section('header', 'پروژه های در دست اجرا')
+@section('content')
+<div class="row">
+    <div class="col-sm-12">
+        <div class="card-box table-responsive">
+
+            <h4 class="header-title m-t-0 m-b-30 inb">
+                لیست پروژه های در دست اجرا
+            </h4>
+
+            <table id="datatable" class="table table-striped table-bordered">
+                <thead>
+                    <tr>
+                        <th>ردیف</th>
+                        <th>عنوان پروژه</th>
+                        <th>توضیحات پروژه (خلاصه)</th>
+                        <th>شناسه پروژه</th>
+                        <th class="tac">مشاهده جزئیات</th>
+                        <th class="tac">ویرایش درصد پیشرفت</th>
+                        <th class="tac">میزان درآمد شما</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    @forelse ($projects as $row => $project)
+                    <tr>
+                        <td><?= $row  + 1 ?></td>
+                        <td class="projectName">{{ $project->title }}</td>
+                        <td>{{ mb_substr($project->description,0 , 80) . "..." }}</td>
+                        <td>{{ $project->unique_id }}</td>
+                        <td class="tac">
+                            <a href="{{ route('contractor.projects.show', $project->id) }}"
+                                class="btn btn-icon waves-effect waves-light btn-primary m-b-5">
+                                <i class="fa fa-file-text-o"></i>
+                            </a>
+                        </td>
+
+
+                        <td class="tac">
+                            <a href="{{ route('contractor.projects.show.progress', $project->id) }}"
+                                class="btn btn-icon waves-effect waves-light btn-info m-b-5"> <i
+                                    class="fa fa-pencil"></i> </a>
+                        </td>
+                        <td class="tac">
+                            <a href="{{ route('earnings.pay', $project->id) }}"
+                                class="btn btn-icon waves-effect waves-light btn-success m-b-5"> <i
+                                    class="fa fa-money"></i> </a>
+                        </td>
+
+                    </tr>
+                    @empty
+                    <div class="alert alert-danger">
+                        <i class="fa fa-warning"></i> 
+                        <span>برای شما تا کنون پروژه ای ثبت نشده است.</span>
+                    </div>
+                    @endforelse
+
+                </tbody>
+            </table>
+            {{ $projects->links() }}
+        </div>
+    </div><!-- end col -->
+</div>
+
+
+@if(session()->has('ProgressChange'))
+<script id="test">
+    var message = "درصد پیشرفت پروژه با موفقیت بروزرسانی شد.";
+    minMbox(message, 350);
+</script>
+    <?php session()->forget('ProgressChange'); ?>
+@endif
+
+
+@endsection
