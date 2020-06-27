564d931561d30d6e0a78fdad8301ed731f20d577
Ali <alighorbani20002@gmail.com>
2020-06-27 15:46:42 +0430

    add helpers file into project

---
+++ app/helpers.php
@@ -0,0 +1,14 @@
+<?php
+
+function hasMember($collection){
+    return is_countable($collection) && count($collection) != 0;
+}
+
+function recordMessage($message = 'موردی جهت نمایش یافت نشد.')
+{
+    $part1 = '<div class="col-lg-offset-2"><img class="exists-record" src="' . asset('admin/images/symbols/cherry.png') . '" alt="record Not Found!"><div class="notfound-content"><div class="notfound-header">';
+    $part2 = '<h3>' . $message . '.</h3>';
+    $part3 = '</div><div class="notfound-body"></div></div></div>';
+    $finalMessage = $part1 . $part2 . $part3;
+    return $finalMessage;
+}
