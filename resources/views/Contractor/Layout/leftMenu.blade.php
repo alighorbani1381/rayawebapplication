4bc546cc0ec3968a0ce4ba062240bcb492a0d2e2
Ali <alighorbani20002@gmail.com>
2020-06-27 11:49:05 +0430

    beauty contractor Panel

--- resources/views/Contractor/Layout/leftMenu.blade.php
+++ resources/views/Contractor/Layout/leftMenu.blade.php
@@ -1,20 +1,21 @@
-   @php
-       $fullName = auth()->user()->name . " " . auth()->user()->lastname;
-   @endphp
-   <!-- ========== Left Sidebar Start ========== -->
-   <div class="left side-menu">
+@php
+$fullName = auth()->user()->name . " " . auth()->user()->lastname;
+@endphp
+<!-- ========== Left Sidebar Start ========== -->
+<div class="left side-menu">
     <div class="sidebar-inner slimscrollleft">
 
         <!-- User -->
         <div class="user-box">
             <div class="user-img">
-                <img src="/admin/images/users/default.png" alt="{{ $fullName }}" title="{{ $fullName }}" class="img-circle img-thumbnail img-responsive">
+                <img src="/admin/images/users/default.png" alt="{{ $fullName }}" title="{{ $fullName }}"
+                    class="img-circle img-thumbnail img-responsive">
                 <div class="user-status online"><i class="zmdi zmdi-dot-circle"></i></div>
             </div>
             <h5>
                 <a href="{{ route('contractor.dashbord') }}">
                     {{ $fullName }}
-                </a> 
+                </a>
             </h5>
             <ul class="list-inline">
                 <li>
@@ -29,28 +30,61 @@
         <!--- Sidemenu -->
         <div id="sidebar-menu">
             <ul>
+
                 <li>
-                    <a href="{{ route('contractor.dashbord') }}" class="waves-effect active"><i class="fa fa-dashboard"></i> <span> داشبورد </span> </a>
+                    <a href="{{ route('contractor.dashbord') }}" class="waves-effect active"><i
+                            class="fa fa-dashboard"></i> <span> داشبورد </span> </a>
                 </li>
 
-              
-                <li class="has_sub">
-                    <a href="javascript:void(0);" class="waves-effect"><i class="fa fa-laptop"></i> <span>پروژه های شما</span> <span class="menu-arrow  fa-angle-left"></span></a>
-                    <ul class="list-unstyled" style="display: none;">
-                        <li><a href="{{ route('contractor.projects.index') }}">لیست پروژه ها</a></li>
-                        <li><a href="{{ route('projects.create') }}">پروژه های در دست اجرا</a></li>
-                        <li><a href="{{ route('projects.create') }}">پروژه های پایان یافته</a></li>
-                    </ul>
+                <li class="text-muted menu-title">بخش کاری</li>
+                <li>
+                    <a href="{{ route('contractor.projects.index') }}" class="waves-effect">
+                        <i class="fa fa-laptop"></i>
+                        <span class="label label-purple pull-right">7</span>
+                        <span> لیست پروژه ها </span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="{{ route('projects.create') }}" class="waves-effect">
+                        <i class="fa fa-codepen"></i>
+                        <span class="label label-warning pull-right">7</span>
+                        <span> پروژه های در دست اجرا </span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="{{ route('projects.create') }}" class="waves-effect">
+                        <i class="fa fa-tags"></i>
+                        <span class="label label-success pull-right">7</span>
+                        <span> پروژه های پایان یافته </span>
+                    </a>
                 </li>
 
 
-                <li class="has_sub">
-                    <a href="javascript:void(0);" class="waves-effect"><i class="fa fa-money"></i> <span>درآمد های شما</span> <span class="menu-arrow  fa-angle-left"></span></a>
-                    <ul class="list-unstyled" style="display: none;">
-                        <li><a href="{{ route('earnings.index') }}">لیست پرداختی ها</a></li>
-                        <li><a href="{{ route('earnings.index') }}">لیست بستانکاری ها</a></li>
-                    </ul>
+                <li class="text-muted menu-title">بخش حسابداری</li>
+
+                <li>
+                    <a href="{{ route('projects.create') }}" class="waves-effect">
+                        <i class="fa fa-money"></i>
+                        <span class="label label-pink pull-right">7</span>
+                        <span> لیست پرداختی ها </span>
+                    </a>
                 </li>
+
+                <li>
+                    <a href="{{ route('projects.create') }}" class="waves-effect">
+                        <i class="fa fa-dollar"></i>
+                        <span class="label label-danger pull-right">7</span>
+                        <span> لیست بستانکاری ها </span>
+                    </a>
+                </li>
+
+
+          
+
+
+          
 
             </ul>
             <div class="clearfix"></div>
