453d1ab0bab256d454201af21ef07d651c94a1b0
Ali <alighorbani20002@gmail.com>
2020-06-27 09:50:40 +0430

    add projects store progress route

--- routes/web.php
+++ routes/web.php
@@ -45,6 +45,7 @@ Route::group(['namespace' => 'Contractor', 'middleware' => ['isLogin']], functio
     Route::get('projects', 'ProjectController@index')->name('contractor.projects.index');
     Route::get('projects/{project}', 'ProjectController@show')->name('contractor.projects.show');
     Route::get('projects/{project}/progress', 'ProjectController@showProgress')->name('contractor.projects.show.progress');
+    Route::post('projects/progress', 'ProjectController@storeProgress')->name('contractor.projects.store.progress');
 });
 
 # Debug or Test Rotue
