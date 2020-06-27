012c9fdfe6975ac02fe6f1c563bd9197df0f3785
Ali <alighorbani20002@gmail.com>
2020-06-27 16:30:39 +0430

    add profile route

--- routes/web.php
+++ routes/web.php
@@ -48,6 +48,7 @@ Route::group(['namespace' => 'Contractor', 'middleware' => ['isLogin']], functio
     Route::get('projects/{project}', 'ProjectController@show')->name('contractor.projects.show');
     Route::get('projects/{project}/progress', 'ProjectController@showProgress')->name('contractor.projects.show.progress');
     Route::post('projects/progress', 'ProjectController@storeProgress')->name('contractor.projects.store.progress');
+    Route::get('profile', 'ProfileController@info')->name('contractor.profile.info');
 });
 
 # Debug or Test Rotue
