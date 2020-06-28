089f1d542c048aa5194a2764f7f06dc6e97876ab
Ali <alighorbani20002@gmail.com>
2020-06-28 08:24:45 +0430

    BackEnd Code  to check password

--- app/Http/Controllers/Contractor/ProfileController.php
+++ app/Http/Controllers/Contractor/ProfileController.php
@@ -24,4 +24,25 @@ class ProfileController extends Controller
         $user = User::where('id', $this->user->id)->first();
         return view('Contractor.Profile.index', compact('user'));
     }
+
+    public function changePassword(Request $request)
+    {
+        $request->validate([
+            'old_password' => 'required',
+            'new_password' => 'required',
+            'repeat_password' => 'required'
+        ]);
+
+        die;
+        $check = $this->checkCurrentPassword($request->old_password);
+        if($check)
+    }
+
+    private function checkCurrentPassword($old)
+    {
+        if(Hash::check($old, $this->password))
+            return true;
+        else
+            return false;
+    }
 }
