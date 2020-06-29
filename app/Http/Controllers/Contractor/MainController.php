fb3efae12b080805856150dcacc328dccb48fc11
Ali <alighorbani20002@gmail.com>
2020-06-29 22:16:22 +0430

    resize after uplode and optimization

--- app/Http/Controllers/Contractor/MainController.php
+++ app/Http/Controllers/Contractor/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Contractor;
 
 use App\Http\Controllers\Controller;
+use Intervention\Image\Facades\Image;
 
 class MainController extends Controller
 {
@@ -14,7 +15,7 @@ class MainController extends Controller
 
     public function imageDelete($path)
     {
-        $path = public_path($path) ;
+        $path = public_path($path);
 
         if (file_exists($path))
             $delete = unlink($path);
@@ -22,13 +23,20 @@ class MainController extends Controller
 
     public function imageUploade($file, $pubpath = null)
     {
-        if($pubpath == null)
+        if ($pubpath == null)
             return null;
 
         $filename = time() . "-" . rand(2, 512) . "." . $file->getClientOriginalExtension();
-        $path = public_path($pubpath) ;
+        $path = public_path($pubpath);
         $files = $file->move($path, $filename);
-        return $filename;
+
+        $img = Image::make($files->getRealPath());
+        $img->resize("512", "512");
+        $img->save($path . "/Profile-" . $filename);
+
+        unlink($files->getRealPath());
+
+        return "/Profile-" . $filename;
     }
 
 
