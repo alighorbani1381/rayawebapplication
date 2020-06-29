fcce8ed3e687baf299553b7e6d53d6b351ae9d1d
Ali <alighorbani20002@gmail.com>
2020-06-29 14:40:08 +0430

    send statistic to the view

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,44 @@
 
 namespace App\Providers;
 
+use App\Repositories\EarningRepository;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use App\Repositories\ProjectRepository;
+use Facade\FlareClient\View;
+use Illuminate\Routing\Console\MiddlewareMakeCommand;
+use Illuminate\Support\Facades\Auth;
 
 class AppServiceProvider extends ServiceProvider
 {
+
+    public function __construct()
+    {
+        $this->projects = resolve(ProjectRepository::class);
+        $this->earning = resolve(EarningRepository::class);
+    }
+
+
+    private function contractorStatistic()
+    {
+        return view()->composer('Contractor.Layout.leftMenu', function ($view) {
+            if (auth()->check()) {
+                $user = auth()->user();
+                $allProjects = $this->projects->getContractorProject($user->id)->count();
+                $ongoingProjects = $this->projects->getContractorOngoingProject($user->id)->count();
+                $finishedProjects = $this->projects->getContractorFinishedProject($user->id)->count();
+                $earning = $this->earning->getContractorEarnings($user->id)->count();
+                $credit = $this->earning->getContractorCredits($user->id)->count();
+                $view->with([
+                    'allProjects' => $allProjects,
+                    'ongoingProjects' => $ongoingProjects,
+                    'finishedProjects' => $finishedProjects,
+                    'earning' => $earning,
+                    'credit' => $credit
+                ]);
+            }
+        });
+    }
     /**
      * Register any application services.
      *
@@ -17,6 +50,8 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 
+
+
     /**
      * Bootstrap any application services.
      *
@@ -24,6 +59,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength('191');     
+        $this->contractorStatistic();
+        Schema::defaultStringLength('191');
     }
 }
