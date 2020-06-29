14e8e61062bae35141d88d9eab6d51fdb4934c8b
Ali <alighorbani20002@gmail.com>
2020-06-29 08:52:21 +0430

    show profile change successful

--- resources/views/Contractor/Index/dashbord.blade.php
+++ resources/views/Contractor/Index/dashbord.blade.php
@@ -4,5 +4,10 @@
 @section('content')
 
 این پنل پیمانکار است 
-
+@if(session()->has('profile-changed'))
+<script>
+    var message = "پروفایل شما با موفقیت تغییر کرد.";
+    minMbox(message, 250);
+</script>
+@endif
 @endsection
