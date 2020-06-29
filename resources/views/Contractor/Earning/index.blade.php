7d247c0f45514f423d6c14e5a25278a6c477acb4
Ali <alighorbani20002@gmail.com>
2020-06-29 10:53:40 +0430

    remove extra stuff and show list of earning

--- resources/views/Contractor/Earning/index.blade.php
+++ resources/views/Contractor/Earning/index.blade.php
@@ -1,9 +1,6 @@
-@extends('Admin.Layout.main')
+@extends('Contractor.Layout.main')
 @section('title', 'لیست در آمد ها')
 @section('header', 'لیست درآمد های شما')
-@push('js')
-<script src="{{ asset('admin/js/customJS/earnings.js') }} "></script>
-@endpush
 @section('content')
 <div class="row">
     <div class="col-sm-12">
@@ -12,22 +9,16 @@
             <h4 class="header-title m-t-0 m-b-30 inb">
                 لیست درآمد های شما</h4>
 
-            <a href="{{ route('earnings.create') }}"
-                class="cbfl btn btn-info btn-bordred waves-effect waves-dark m-b-5"> <i class="fa fa-plus-circle"></i>
-                <span>افزودن جدید </span> </a>
-
-
             <table id="datatable" class="table table-striped table-bordered">
                 <thead>
                     <tr>
                         <th>ردیف</th>
                         <th>عنوان درآمد</th>
+                        <th>ثبت کننده این درآمد</th>
                         <th>عنوان پروژه</th>
                         <th>شناسه پروژه</th>
                         <th>میزان درآمد (تومان)</th>
                         <th class="tac">جزئیات</th>
-                        <th class="tac">ویرایش</th>
-                        <th class="tac">حذف</th>
                     </tr>
                 </thead>
 
@@ -36,39 +27,14 @@
                     <tr>
                         <td><?= $row  + 1 ?></td>
                         <td>{{ $earning->title }}</td>
+                        <td>{{ $earning->name . " " . $earning->lastname  }}</td>
                         <td>{{ $earning->project_title }}</td>
                         <td>{{ $earning->unique_id }}</td>
-                        <td>{{ number_format($earning->received_money) }}</td>
-                        <td>{{ number_format($earning->price) }}</td>
-                        <td>
-                            @if($earning->status == 'paid')
-                            <button type="button"
-                                class="btn btn-success btn-rounded w-md waves-effect waves-light m-b-5">پرداخت
-                                شده</button>
-                            @else
-                            <button type="button"
-                                class="btn btn-danger btn-rounded w-md waves-effect waves-light m-b-5">پرداخت
-                                نشده</button>
-                            @endif
-                        </td>
+                        <td>{{ number_format($earning->money_paid) }}</td>
                         <td class="tac">
                             <a href="{{ route('earnings.show', $earning->id) }}"
                                 class="btn btn-icon waves-effect waves-light btn-primary m-b-5"> <i
                                     class="fa fa-file-text-o"></i> </a>
-                        </td>
-                        <td class="tac">
-                            <a href="{{ route('earnings.edit', $earning->id) }}"
-                                class="btn btn-icon waves-effect waves-light btn-info m-b-5"> <i
-                                    class="fa fa-pencil"></i> </a>
-                        </td>
-                        <td class="tac">
-                            <form method="post" action="{{ route('earnings.destroy', $earning->id) }}">
-                                @csrf
-                                @method('DELETE')
-                                <button type="button"
-                                    class="delete-earning btn btn-icon waves-effect waves-light btn-danger m-b-5"> <i
-                                        class="fa fa-remove"></i> </button>
-                            </form>
                         </td>
                     </tr>
                     @endforeach
@@ -78,19 +44,6 @@
 
             {{ $earnings->links() }}
         </div>
-    </div><!-- end col -->
+    </div>
 </div>
-
-@if(session()->has('DeleteEarning'))
-<script id="showMessage">
-    minMbox('درآمد مورد نظر با موفقیت حذف شد.', 350);
-    setTimeout(function(){$("#showMessage").remove();}, 400);
-</script>
-@endif
-
-@if(session()->has('UpdateEarning'))
-<script>
-    minMbox('درآمد مورد نظر با موفقیت ویرایش شد.', 350);
-</script>
-@endif
 @endsection
