6cc17de4fbb72b1e4b737a4f2ae09cedb9e4dfca
Ali <alighorbani20002@gmail.com>
2020-06-29 13:22:27 +0430

    add project method to get earning from project

--- app/Http/Controllers/Contractor/EarningController.php
+++ app/Http/Controllers/Contractor/EarningController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Contractor;
 
 use App\Http\Controllers\Controller;
+use App\Project;
 use App\Repositories\EarningRepository;
 use Illuminate\Http\Request;
 
@@ -16,7 +17,7 @@ class EarningController extends Controller
         $this->repo = resolve(EarningRepository::class);
 
         # Encapsulation User
-        $this->middleware(function($request, $next){
+        $this->middleware(function ($request, $next) {
             $this->user = auth()->user();
             return $next($request);
         });
@@ -30,13 +31,20 @@ class EarningController extends Controller
 
     public function show($earning)
     {
-        $earning = $this->repo->getContractorEarning($earning);   
+        $earning = $this->repo->getContractorEarning($earning);
         return view('Contractor.Earning.show', compact('earning'));
     }
 
     public function credit()
     {
-        $credits = $this->repo->getContractorCredits($this->user->id);   
+        $credits = $this->repo->getContractorCredits($this->user->id);
         return view('Contractor.Earning.credit', compact('credits'));
+    }
+
+    public function project(Project $project)
+    {
+        $earnings = $this->repo->getContractorProjectEarnings($project->id, $this->user->id);
+        dd($earnings);
+        return view('Contractor.Earning.projects', compact('project', 'earnings'));
     }
 }
