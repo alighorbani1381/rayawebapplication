be056532c61a7852d80cbffdfa8cec42afd80dfd
Ali <alighorbani20002@gmail.com>
2020-06-30 14:29:28 +0430

    add helpful method

--- app/Category.php
+++ app/Category.php
@@ -7,31 +7,36 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     protected $guarded = [];
-    
+
     public function getSubDescAttribute()
     {
         return mb_substr($this->description, 0, 50) . " ...";
     }
-        
+
     public function getMainGroupAttribute()
     {
-        if ($this->child != 0){
+        if ($this->child != 0) {
             $mainCategory = Category::where('id', $this->child)->first();
             $mainCategory = $mainCategory->title;
-        }
-        else
+        } else
             $mainCategory = "ندارد";
 
-            return $mainCategory;
+        return $mainCategory;
     }
 
     public function getSubCatsAttribute()
     {
-        if ($this->child == 0)
-            $subCategory = Category::where('child', $this->id)->get();        
-        else
-            $subCategory = [];
+        $subCategory = ($this->child == 0) ? Category::where('child', $this->id)->get() : [];
+        return $subCategory;
+    }
 
-            return $subCategory;
+    public function hasSubCategory()
+    {
+        return ($this->sub_cats->count() != 0) ? true : false;
+    }
+
+    public function getCountSub()
+    {
+        return $this->sub_cats->count();
     }
 }
