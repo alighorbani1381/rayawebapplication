750cd287a4dc6da202e8323ac1a11ddd11e32846
Ali <alighorbani20002@gmail.com>
2020-06-30 08:46:40 +0430

    add chagne method into profile controller

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Repositories\ProfileRepository;
 use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileController extends AdminController
 {
     private $user;
+
+    private $repo;
 
     public function __construct()
     {
@@ -14,6 +19,8 @@ class ProfileController extends AdminController
             $this->user = auth()->user();
             return $next($request);
         });
+
+        $this->repo = resolve(ProfileRepository::class);
     }
 
     public function index()
@@ -21,4 +28,26 @@ class ProfileController extends AdminController
         $user = User::where('id', $this->user->id)->first();
         return view('Admin.User.profile', compact('user'));
     }
+
+    public function changePassword(Request $request)
+    {
+        // return $request->all();
+
+        $request->validate([
+            'old_password' => 'required',
+            'new_password' => 'required',
+            'repeat_password' => 'required',
+        ]);
+
+        if (!$this->repo->isValidPassword($request->old_password, $this->user->password)) {
+            return back();
+        }
+
+        $oldPass = $request->old_password;
+        $newPass = $request->new_password;
+        $reaptPass = $request->repeat_password;
+        return $this->repo->isValidNewPassword($oldPass, $newPass, $reaptPass, $this->user->id);
+    }
+
+   
 }
