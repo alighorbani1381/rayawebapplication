4b0166040a88c7a144d14e811ad51d92659252cf
Ali <alighorbani20002@gmail.com>
2020-06-30 08:47:12 +0430

    add project repository

---
+++ app/Repositories/ProfileRepository.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Repositories;
+
+use App\User;
+use Illuminate\Support\Facades\Hash;
+
+
+class ProfileRepository{
+
+    public function isValidPassword($oldPass, $currentPass)
+    {
+        
+        $isValid = true;
+
+        if (! Hash::check($oldPass, $currentPass)){
+            session()->flash('currentWrong');
+            $isValid = false;
+        }
+
+        return $isValid;
+    }
+
+    public function isValidNewPassword($oldPass, $newPass, $repeatPass, $userId)
+    {
+        $user = User::where('id', $userId)->first();
+        $isValid =  ($newPass == $repeatPass) ? true : false;
+        if (!$isValid) {
+            session()->flash('newpass-Wrong', $oldPass);
+            return back();
+        }
+        User::where('id', $userId)
+            ->update(['password' =>  Hash::make($newPass)]);
+        session()->flash('changed-password');
+
+        if ($user == 'admin')
+            return redirect()->route('admin.dashbord');
+        else
+            return redirect()->route('contractor.dashbord');
+    }
+
+}
