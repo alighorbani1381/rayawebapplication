7696dfe77e92910cc21b44f773bf74cbf2a62a57
Ali <alighorbani20002@gmail.com>
2020-07-01 11:27:28 +0430

    getProjectsCount in repo

--- app/Repositories/CategoryRepository.php
+++ app/Repositories/CategoryRepository.php
@@ -2,9 +2,24 @@
 
 namespace App\Repositories;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
 
 class CategoryRepository
 {
+    const CATEGORY_PROJECT = "project_category";
 
+    public $categoryId;
+
+    public function getCategoryId()
+    {
+        return $this->categoryId;
+    }
+
+    public function getProjectsCount()
+    {
+        return DB::table(self::CATEGORY_PROJECT)
+        ->where('category_id', $this->getCategoryId())
+        ->count();
+    }
 }
