b8f7cb8a738a86c972a2c5821ab86df4525480f5
Ali <alighorbani20002@gmail.com>
2020-07-01 11:26:46 +0430

    add some muator to category

--- app/Category.php
+++ app/Category.php
@@ -2,11 +2,20 @@
 
 namespace App;
 
+use App\Repositories\CategoryRepository;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
     protected $guarded = [];
+
+    public $repo;
+
+
+    public function __construct()
+    {
+        $this->repo = resolve(CategoryRepository::class);
+    }
 
     public function getSubDescAttribute()
     {
@@ -30,13 +39,28 @@ class Category extends Model
         return $subCategory;
     }
 
+
+
+    #return it has Sub Category
     public function hasSubCategory()
     {
-        return ($this->sub_cats->count() != 0) ? true : false;
+        return (count($this->sub_cats) != 0) ? true : false;
     }
 
+    # return Count Sub Category    
     public function getCountSub()
     {
-        return $this->sub_cats->count();
+        return count($this->sub_cats);
+    }
+
+    public function getProjectCount()
+    {
+        $this->repo->categoryId = $this->id;
+        return $this->repo->getProjectsCount();
+    }
+
+    public function hasProjects()
+    {
+        return ($this->getProjectCount() != 0) ? true : false;
     }
 }
