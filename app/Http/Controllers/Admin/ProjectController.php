1aeeaaea626645f6563817502f1d4efad96cdd26
Ali <alighorbani20002@gmail.com>
2020-07-01 14:18:04 +0430

    remove request class from controller class

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -4,40 +4,10 @@ namespace App\Http\Controllers\Admin;
 
 use App\Project;
 use App\Repositories\ProjectRepository;
+use App\Request\ProjectRequest;
 use Illuminate\Http\Request;
 
-class ProjectRequest
-{
 
-    public static function projectValidate($request)
-    {
-        $fileds = [
-            'name'             => 'required',
-            'lastname'         => 'required',
-            'father_name'      => 'required',
-            'meli_code'        => 'required',
-            'meli_image'       => 'default',
-            'phone'            => 'required',
-            'address'          => 'required',
-            'title'            => 'required',
-            'description'      => 'required',
-            'price'            => 'required',
-            'contract_image'   => 'default',
-            'contract_started' => 'required|before:' . $request->completed_at,
-            'completed_at'     => 'required|after:' . $request->contract_started,
-            'date_start'       => 'required',
-            'complete_after'   => 'required|numeric|min:1',
-        ];
-
-        $request->validate($fileds);
-    }
-
-    public static function percentValidate($request)
-    {
-        $fileds = ['progress.*' => 'required|numeric|min:1|max:100'];
-        $request->validate($fileds);
-    }
-}
 
 
 class ProjectController extends AdminController
