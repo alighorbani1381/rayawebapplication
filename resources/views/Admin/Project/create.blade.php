1eacab2ba6d58c894b74ed6dce41ac405b2770bb
Ali <alighorbani20002@gmail.com>
2020-07-03 08:24:26 +0430

    edit file add required and fixed size

--- resources/views/Admin/Project/create.blade.php
+++ resources/views/Admin/Project/create.blade.php
@@ -1,27 +1,9 @@
 @extends('Admin.Layout.main')
 @section('title', 'افزودن پروژه')
 @section('header', 'ایجاد پروژه')
-@push('css')
-<link href="{{ asset('admin/css/persian-datepicker.css') }}" rel="stylesheet" type="text/css" />
-@endpush
-@push('js')
-<script src="{{ asset('admin/js/customJS/projects.js') }} "></script>
-<script src="{{ asset('admin/js/persian-date.min.js') }} "></script>
-<script src="{{ asset('admin/js/persian-datepicker.js') }} "></script>
-<script type="text/javascript">
-    $(document).ready(function() {
-      $(".persian-date").persianDatepicker({
-        minDate: new persianDate().unix(),
-        format: 'YYYY/MM/DD',	
-        initialValueType: 'persian',
-        autoClose: true,
-        });
-    });
-</script>
-@endpush
 @section('content')
 <div class="row">
-    <div class="col-sm-10 col-lg-offset-1">
+    <div class="col-sm-10 col-sm-offset-1">
         <div class="card-box">
             <div class="row">
                 <form class="form-vertical" method="post" action="{{ route('projects.store') }}"
@@ -37,8 +19,8 @@
                         <div class="form-group">
                             <label class="col-md-3 control-label">عنوان پروژه</label>
                             <div class="col-md-9">
-                                <input type="text" name="title" required class="form-control"
-                                    value="{{ old ('title') }}" placeholder="عنوان پروژه را وارد کنید ...">
+                                <input type="text" name="title"  class="form-control"
+                                    value="{{ old ('title') }}" placeholder="عنوان پروژه را وارد کنید ..." required>
                                 @error('title')
                                 <div class="alert alert-danger"> {{ $message }} </div>
                                 @enderror
@@ -72,7 +54,7 @@
                             <label class="col-md-3 control-label">توضیحات پروژه</label>
                             <div class="col-md-9">
                                 <textarea class="form-control txt-custom"
-                                    placeholder="توضیحات مربوط به پروژه را وارد کنید ..." name="description"
+                                    placeholder="توضیحات مربوط به پروژه را وارد کنید ..." name="description" required
                                     rows="5">{{ old('description') }}</textarea>
                                 @error('description')
                                 <div class="alert alert-danger"> {{ $message }} </div>
@@ -82,7 +64,7 @@
                         <div class="form-group">
                             <label class="col-md-3 control-label">قیمت (تومان)</label>
                             <div class="col-md-9">
-                                <input type="number" required placeholder="قیمت این پروژه را وارد کنید ..." name="price"
+                                <input type="number"  placeholder="قیمت این پروژه را وارد کنید ..." name="price" required
                                     class="form-control" value="{{ old ('price') }}" id="price">
 
                                 @error('price')
@@ -118,9 +100,9 @@
                         <div class="form-group">
                             <label class="col-md-3 control-label">تاریخ شروع کار </label>
                             <div class="col-md-9">
-                                <input type="text" required placeholder="تاریخ شروع کار را وارد کنید ..."
+                                <input type="text" placeholder="تاریخ شروع کار را وارد کنید ..."
                                     name="date_start" class="form-control persian-date"
-                                    value="{{ old ('date_start') }}">
+                                    value="{{ old ('date_start') }}" required>
                                 @error('date_start')
                                 <div class="alert alert-danger"> {{ $message }} </div>
                                 @enderror
@@ -130,8 +112,8 @@
                         <div class="form-group">
                             <label class="col-md-3 control-label">زمان تحویل پروژه (روز)</label>
                             <div class="col-md-9">
-                                <input type="number" required placeholder="زمان تحویل پروژه بر حسب روز را وارد کنید ..."
-                                    name="complete_after" class="form-control" value="{{ old ('complete_after') }}">
+                                <input type="number" placeholder="زمان تحویل پروژه بر حسب روز را وارد کنید ..."
+                                    name="complete_after" class="form-control" value="{{ old ('complete_after') }}" required>
                                 @error('complete_after')
                                 <div class="alert alert-danger"> {{ $message }} </div>
                                 @enderror
@@ -158,8 +140,8 @@
                     <div class="form-group">
                         <label class="col-md-3 control-label">نام</label>
                         <div class="col-md-9">
-                            <input type="text" required name="name" class="form-control" value="{{ old ('name') }}"
-                                placeholder="نام کارفرما را وارد کنید ...">
+                            <input type="text" name="name" class="form-control" value="{{ old ('name') }}"
+                                placeholder="نام کارفرما را وارد کنید ..." required>
                             @error('name')
                             <div class="alert alert-danger"> {{ $message }} </div>
                             @enderror
@@ -169,8 +151,8 @@
                     <div class="form-group">
                         <label class="col-md-3 control-label">نام خانوادگی</label>
                         <div class="col-md-9">
-                            <input type="text" required name="lastname" class="form-control"
-                                value="{{ old ('lastname') }}" placeholder="نام خانوادگی کارفرما را وارد کنید ...">
+                            <input type="text" name="lastname" class="form-control"
+                                value="{{ old ('lastname') }}" placeholder="نام خانوادگی کارفرما را وارد کنید ..." required>
                             @error('lastname')
                             <div class="alert alert-danger"> {{ $message }} </div>
                             @enderror
@@ -180,8 +162,8 @@
                     <div class="form-group">
                         <label class="col-md-3 control-label">نام پدر</label>
                         <div class="col-md-9">
-                            <input type="text" required name="father_name" class="form-control"
-                                value="{{ old ('father_name') }}" placeholder="نام پدر کارفرما را وارد کنید ...">
+                            <input type="text" name="father_name" class="form-control"
+                                value="{{ old ('father_name') }}" placeholder="نام پدر کارفرما را وارد کنید ..." required>
                             @error('father_name')
                             <div class="alert alert-danger"> {{ $message }} </div>
                             @enderror
@@ -191,8 +173,8 @@
                     <div class="form-group">
                         <label class="col-md-3 control-label">شماره تماس کارفرما</label>
                         <div class="col-md-9">
-                            <input type="text" required name="phone" class="form-control" value="{{ old ('phone') }}"
-                                placeholder="شماره تماس کارفرما را وارد کنید ...">
+                            <input type="text"  name="phone" class="form-control" value="{{ old ('phone') }}"
+                                placeholder="شماره تماس کارفرما را وارد کنید ..." required>
                             @error('phone')
                             <div class="alert alert-danger"> {{ $message }} </div>
                             @enderror
@@ -204,9 +186,9 @@
                     <div class="form-group">
                         <label class="col-md-3 control-label">آدرس دقیق کارفرما</label>
                         <div class="col-md-9">
-                            <textarea class="form-control txt-custom" required
+                            <textarea class="form-control txt-custom" 
                                 placeholder="آدرس کارفرما را به صورت دقیق وارد کنید ..." name="address"
-                                rows="3">{{ old('address') }}</textarea>
+                                rows="3" required>{{ old('address') }}</textarea>
                             @error('address')
                             <div class="alert alert-danger"> {{ $message }} </div>
                             @enderror
@@ -216,8 +198,8 @@
                     <div class="form-group">
                         <label class="col-md-3 control-label">کد ملی</label>
                         <div class="col-md-9">
-                            <input type="text" required name="meli_code" class="form-control"
-                                value="{{ old ('address') }}" placeholder="کد ملی کارفرما را وارد کنید ...">
+                            <input type="text" name="meli_code" class="form-control"
+                                value="{{ old ('address') }}" placeholder="کد ملی کارفرما را وارد کنید ..." required>
                             @error('meli_code')
                             <div class="alert alert-danger"> {{ $message }} </div>
                             @enderror
@@ -227,7 +209,7 @@
                     <div class="form-group">
                         <label class="col-md-3 control-label">تصویر کد ملی</label>
                         <div class="col-md-9">
-                            <input type="file" name="meli_image">
+                            <input type="file" name="meli_image" required>
                             @error('meli_image')
                             <div class="alert alert-danger" style="margin: 8px 0;"> {{ $message }} </div>
                             @enderror
@@ -250,9 +232,9 @@
                     <div class="form-group">
                         <label class="col-md-3 control-label">تاریخ شروع قرارداد </label>
                         <div class="col-md-9">
-                            <input type="text" required placeholder="تاریخ شروع قرارداد را وارد کنید ..."
+                            <input type="text" placeholder="تاریخ شروع قرارداد را وارد کنید ..."
                                 name="contract_started" class="form-control persian-date"
-                                value="{{ old ('contract_started') }}">
+                                value="{{ old ('contract_started') }}" required>
                             @error('contract_started')
                             <div class="alert alert-danger"> {{ $message }} </div>
                             @enderror
@@ -262,9 +244,9 @@
                     <div class="form-group">
                         <label class="col-md-3 control-label">تاریخ پایان قرارداد </label>
                         <div class="col-md-9">
-                            <input type="text" required placeholder="تاریخ پایان قرارداد را وارد کنید ..."
+                            <input type="text" placeholder="تاریخ پایان قرارداد را وارد کنید ..."
                                 name="completed_at" class="form-control persian-date"
-                                value="{{ old ('completed_at') }}">
+                                value="{{ old ('completed_at') }}" required>
                             @error('completed_at')
                             <div class="alert alert-danger"> {{ $message }} </div>
                             @enderror
@@ -274,7 +256,7 @@
                     <div class="form-group">
                         <label class="col-md-3 control-label">تصویر قرارداد</label>
                         <div class="col-md-9">
-                            <input type="file" name="contract_image">
+                            <input type="file" name="contract_image" required>
                             @error('contract_image')
                             <div class="alert alert-danger"  style="margin: 8px 0;"> {{ $message }} </div>
                             @enderror
@@ -305,3 +287,22 @@ maxMbox(title, message, 'warning', btnText, 250);
 </script>
 @endif
 @endsection
+
+@push('css')
+<link href="{{ asset('admin/css/persian-datepicker.css') }}" rel="stylesheet" type="text/css">
+@endpush
+@push('js')
+<script src="{{ asset('admin/js/customJS/projects.js') }} "></script>
+<script src="{{ asset('admin/js/persian-date.min.js') }} "></script>
+<script src="{{ asset('admin/js/persian-datepicker.js') }} "></script>
+<script type="text/javascript">
+    $(document).ready(function() {
+      $(".persian-date").persianDatepicker({
+        minDate: new persianDate().unix(),
+        format: 'YYYY/MM/DD',	
+        initialValueType: 'persian',
+        autoClose: true,
+        });
+    });
+</script>
+@endpush
