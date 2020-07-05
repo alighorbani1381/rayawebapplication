f492c6fe83986840dfa51aa090532e08243ef94e
Ali <alighorbani20002@gmail.com>
2020-07-05 12:36:13 +0430

    format code and fix

--- resources/views/Contractor/Earning/index.blade.php
+++ resources/views/Contractor/Earning/index.blade.php
@@ -3,53 +3,51 @@
 @section('header', 'لیست درآمد های شما')
 @section('content')
 <div class="row">
-    <div class="col-sm-12">
-        <div class="card-box table-responsive">
-
-            <h4 class="header-title m-t-0 m-b-30 inb">
-                لیست درآمد های شما</h4>
-            @if(hasMember($earnings))
-            <table id="datatable" class="table table-striped table-bordered">
-                <thead>
-                    <tr>
-                        <th>ردیف</th>
-                        <th>عنوان درآمد</th>
-                        <th>ثبت کننده این درآمد</th>
-                        <th class="tac">تاریخ ثبت درآمد</th>
-                        <th class="tac">زمان ثبت درآمد</th>
-                        <th>عنوان پروژه</th>
-                        <th>شناسه پروژه</th>
-                        <th>میزان درآمد (تومان)</th>
-                        <th class="tac">جزئیات</th>
-                    </tr>
-                </thead>
-
-                <tbody>
-                    @foreach ($earnings as $row => $earning)
-                    <tr>
-                        <td><?= $row  + 1 ?></td>
-                        <td>{{ $earning->title }}</td>
-                        <td>{{ $earning->name . " " . $earning->lastname  }}</td>
-                        <td class="tac date-show">{{ verta($earning->created_at)->formatJalaliDate() }}</td>
-                        <td class="tac date-show">{{ verta($earning->created_at)->format('h:m') }}</td>
-                        <td>{{ $earning->project_title }}</td>
-                        <td>{{ $earning->unique_id }}</td>
-                        <td>{{ number_format($earning->money_paid) }}</td>
-                        <td class="tac">
-                            <a href="{{ route('contractor.earning.show', $earning->id) }}"
-                                class="btn btn-icon waves-effect waves-light btn-primary m-b-5"> <i
-                                    class="fa fa-file-text-o"></i> </a>
-                        </td>
-                    </tr>
-                    @endforeach
-
-                </tbody>
-            </table>
-            @else
-            {!! recordMessage("برای شما تا کنون درآمدی ثبت نشده است") !!}
-            @endif
-            {{ $earnings->links() }}
-        </div>
-    </div>
+   <div class="col-sm-12">
+      <div class="card-box table-responsive">
+         @if(hasMember($earnings))
+         <h4 class="header-title m-t-0 m-b-30 inb">
+            لیست درآمد های شما
+         </h4>
+         <table id="datatable" class="table table-striped table-bordered">
+            <thead>
+               <tr>
+                  <th>ردیف</th>
+                  <th>عنوان درآمد</th>
+                  <th>ثبت کننده این درآمد</th>
+                  <th class="tac">تاریخ ثبت درآمد</th>
+                  <th class="tac">زمان ثبت درآمد</th>
+                  <th>عنوان پروژه</th>
+                  <th>شناسه پروژه</th>
+                  <th>میزان درآمد (تومان)</th>
+                  <th class="tac">جزئیات</th>
+               </tr>
+            </thead>
+            <tbody>
+               @foreach ($earnings as $row => $earning)
+               <tr>
+                  <td><?= $row  + 1 ?></td>
+                  <td>{{ $earning->title }}</td>
+                  <td>{{ $earning->name . " " . $earning->lastname  }}</td>
+                  <td class="tac date-show">{{ verta($earning->created_at)->formatJalaliDate() }}</td>
+                  <td class="tac date-show">{{ verta($earning->created_at)->format('h:m') }}</td>
+                  <td>{{ $earning->project_title }}</td>
+                  <td>{{ $earning->unique_id }}</td>
+                  <td>{{ number_format($earning->money_paid) }}</td>
+                  <td class="tac">
+                     <a href="{{ route('contractor.earning.show', $earning->id) }}"
+                        class="btn btn-icon waves-effect waves-light btn-primary m-b-5"> <i class="fa fa-file-text-o"></i> 
+                     </a>        
+                  </td>
+               </tr>
+               @endforeach
+            </tbody>
+         </table>
+         @else
+         {!! recordMessage("برای شما تا کنون درآمدی ثبت نشده است") !!}
+         @endif
+         {{ $earnings->links() }}
+      </div>
+   </div>
 </div>
 @endsection
