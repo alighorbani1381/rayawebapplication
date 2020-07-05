25d11554872c08f8f55cdd3555299c0f945cf495
Ali <alighorbani20002@gmail.com>
2020-07-05 13:01:55 +0430

    fixed formatter category modul

--- resources/views/Admin/Category/create.blade.php
+++ resources/views/Admin/Category/create.blade.php
@@ -3,60 +3,65 @@
 @section('header', 'افزودن خدمات جدید')
 @section('content')
 <div class="row">
-    <div class="col-sm-8 col-lg-offset-2">
-        <div class="card-box">
-            <h4 class="header-title m-t-0 m-b-30">افزودن خدمت جدید</h4>
-            <div class="row">
-                <div class="col-lg-11">
-                    <form class="form-horizontal" role="form" action="{{ route('categories.store') }}" method="post">
-                        @csrf
-                        <div class="form-group">
-                            <label class="col-md-2 control-label">عنوان خدمات</label>
-                            <div class="col-md-10">
-                                <input type="text" name="title" required class="form-control" value="{{ old('title') }}"
-                                    placeholder="عنوان خدمت را وارد کنید ...">
-                                @error('title')
-                                    <div class="alert alert-danger">{{ $message }}</div>
-                                @enderror
-                            </div>
-                        </div>
+   <div class="col-sm-8 col-lg-offset-2">
+      <div class="card-box">
+         <h4 class="header-title m-t-0 m-b-30">افزودن خدمت جدید</h4>
+         <div class="row">
+            <div class="col-lg-11">
+               <form class="form-horizontal" role="form" action="{{ route('categories.store') }}" method="post">
+                  @csrf
 
-                        <div class="form-group">
-                            <label class="col-md-2 control-label">توضیحات</label>
-                            <div class="col-md-10">
-                                <textarea class="form-control txt-custom" required placeholder="توضیحات خدمت را وارد کنید ..."
-                                    name="description" rows="5">{{ old('description') }}</textarea>
-                                @error('description')
-                                  <div class="alert alert-danger">{{ $message }}</div>
-                                @enderror
-                            </div>
-                        </div>
+                  <!-- Title Box !-->
+                  <div class="form-group">
+                     <label class="col-md-2 control-label">عنوان خدمات</label>
+                     <div class="col-md-10">
+                        <input type="text" name="title" required class="form-control" value="{{ old('title') }}"
+                           placeholder="عنوان خدمت را وارد کنید ...">
+                        @error('title')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                     </div>
+                  </div>
 
+                  <!-- Description Box !-->
+                  <div class="form-group">
+                     <label class="col-md-2 control-label">توضیحات</label>
+                     <div class="col-md-10">
+                        <textarea class="form-control txt-custom" required placeholder="توضیحات خدمت را وارد کنید ..."
+                           name="description" rows="5">{{ old('description') }}</textarea>
+                        @error('description')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                     </div>
+                  </div>
 
-                        <div class="form-group">
-                            <label class="col-md-2 control-label">سرگروه</label>
-                            <div class="col-sm-10">
-                                <select class="form-control" name="child" required>
-                                    <option value="0" selected>ندارد</option>
-                                    @foreach($mainCategories as $category)
-                                    <option value="{{ $category->id }}">{{ $category->title}} </option>
-                                    @endforeach
-                                </select>
-                                @error('child')
-                                    <div class="alert alert-danger">{{ $description }}</div>
-                                @enderror
-                            </div>
-                        </div>
+                  <!-- Subset Box !-->
+                  <div class="form-group">
+                     <label class="col-md-2 control-label">سرگروه</label>
+                     <div class="col-sm-10">
+                        <select class="form-control" name="child" required>
+                           <option value="0" selected>ندارد</option>
+                           @foreach($mainCategories as $category)
+                           <option value="{{ $category->id }}">{{ $category->title}} </option>
+                           @endforeach
+                        </select>
+                        @error('child')
+                        <div class="alert alert-danger">{{ $description }}</div>
+                        @enderror
+                     </div>
+                  </div>
 
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-success waves-effect waves-light submit-button">افزودن
-                                خدمت</button>
-                        </div>
-                    </form>
-                </div>
+                  <!-- Submit Button !-->
+                  <div class="form-group">
+                     <button type="submit" class="btn btn-success waves-effect waves-light submit-button">
+                        افزودن خدمت
+                    </button>
+                  </div>
 
+               </form>
             </div>
-        </div>
-    </div>
+         </div>
+      </div>
+   </div>
 </div>
 @endsection
--- resources/views/Admin/Category/index.blade.php
+++ resources/views/Admin/Category/index.blade.php
@@ -6,83 +6,84 @@
 @endpush
 @section('content')
 <div class="row">
-    <div class="col-sm-12">
-        <div class="card-box table-responsive">
+   <div class="col-sm-12">
+      <div class="card-box table-responsive">
+         @if(hasMember($categories))
+         <h4 class="header-title m-t-0 m-b-30 inb">
+            لیست خدمات شما
+         </h4>
+         <a href="{{ route('categories.create') }}"
+            class="cbfl btn btn-info btn-bordred waves-effect waves-dark m-b-5"> <i class="fa fa-plus-circle"></i>
+         <span>افزودن جدید </span>
+         </a>
 
-            <h4 class="header-title m-t-0 m-b-30 inb">
-                لیست خدمات شما</h4>
+         <!-- Table Start !-->
+         <table id="datatable" class="table table-striped table-bordered">
+            <thead>
+               <tr>
+                  <th>ردیف</th>
+                  <th>عنوان خدمت</th>
+                  <th>توضیحات (خلاصه)</th>
+                  <th>سرگروه</th>
+                  <th class="tac">ویرایش</th>
+                  <th class="tac">حذف</th>
+               </tr>
+            </thead>
+            <tbody>
+               @foreach ($categories as $row => $category)
+               <tr>
+                  <td><?= $row  + 1 ?></td>
+                  <td class="categoryName">{{ $category->title }}</td>
+                  <td>{{ $category->sub_desc }}</td>
+                  <td>{{ $category->main_group }}</td>
+                  <td class="tac">
+                     <a href="{{ route('categories.edit', $category->id) }}"
+                        class="btn btn-icon waves-effect waves-light btn-info m-b-5"> <i
+                        class="fa fa-pencil"></i> </a>
+                  </td>
+                  <td class="tac">
+                     <form method="post" action="{{ route('categories.destroy', $category->id) }}">
+                        @csrf
+                        @method('DELETE')
+                        <button type="button"
+                           class="delete-button btn btn-icon waves-effect waves-light btn-danger m-b-5"> <i
+                           class="fa fa-remove"></i> </button>
+                     </form>
+                  </td>
+               </tr>
+               @endforeach
+            </tbody>
+         </table>
+         <!-- Table End !-->
 
-            <a href="{{ route('categories.create') }}"
-                class="cbfl btn btn-info btn-bordred waves-effect waves-dark m-b-5"> <i class="fa fa-plus-circle"></i>
-                <span>افزودن جدید </span> </a>
-
-            @if(hasMember($categories))
-            <table id="datatable" class="table table-striped table-bordered">
-                <thead>
-                    <tr>
-                        <th>ردیف</th>
-                        <th>عنوان خدمت</th>
-                        <th>توضیحات (خلاصه)</th>
-                        <th>سرگروه</th>
-                        <th class="tac">ویرایش</th>
-                        <th class="tac">حذف</th>
-                    </tr>
-                </thead>
-
-                <tbody>
-                    @foreach ($categories as $row => $category)
-                    <tr>
-                        <td><?= $row  + 1 ?></td>
-                        <td class="categoryName">{{ $category->title }}</td>
-                        <td>{{ $category->sub_desc }}</td>
-                        <td>{{ $category->main_group }}</td>
-                        <td class="tac">
-                            <a href="{{ route('categories.edit', $category->id) }}"
-                                class="btn btn-icon waves-effect waves-light btn-info m-b-5"> <i
-                                    class="fa fa-pencil"></i> </a>
-                        </td>
-                        <td class="tac">
-                            <form method="post" action="{{ route('categories.destroy', $category->id) }}">
-                                @csrf
-                                @method('DELETE')
-                                <button type="button"
-                                    class="delete-button btn btn-icon waves-effect waves-light btn-danger m-b-5"> <i
-                                        class="fa fa-remove"></i> </button>
-                            </form>
-                        </td>
-                    </tr>
-                    @endforeach
-
-                </tbody>
-            </table>
-            @else
-            {!! recordMessage('دسته بندی برای نمایش وجود ندارد') !!}
-            @endif
-        </div>
-    </div><!-- end col -->
+         @else
+         {!! recordMessage('دسته بندی برای نمایش وجود ندارد') !!}
+         @endif
+      </div>
+   </div>
 </div>
+
 @if(session()->has('Error-Sub'))
 <script>
-    var title ="حذف این مورد با شکست مواجه شد!";
-    var part1 = "این خدمت دارای" + " {{Session::get('Error-Sub')}} " + "زیر";
-    var part2 ="گروه است و نمی توان آن را حذف کرد";
-    var message = part1 + part2;
-    var btn = "آها";
-
-    maxMbox(title, message, "error", btn, 350 );
+   var title ="حذف این مورد با شکست مواجه شد!";
+   var part1 = "این خدمت دارای" + " {{Session::get('Error-Sub')}} " + "زیر";
+   var part2 ="گروه است و نمی توان آن را حذف کرد";
+   var message = part1 + part2;
+   var btn = "آها";
+   maxMbox(title, message, "error", btn, 350 );
 </script>
 @endif
 
-
 @if(session()->has('DeleteCategory'))
 <script>
-    minMbox('خدمت مورد نظر با موفقیت حذف شد.', 350);
+   minMbox('خدمت مورد نظر با موفقیت حذف شد.', 350);
 </script>
 @endif
 
 @if(session()->has('CategoryUpdate'))
 <script>
-    minMbox('خدمت مورد نظر با موفقیت ویرایش شد.', 350);
+   minMbox('خدمت مورد نظر با موفقیت ویرایش شد.', 350);
 </script>
 @endif
+
 @endsection
