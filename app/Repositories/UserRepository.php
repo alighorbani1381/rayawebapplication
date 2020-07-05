6a387b2476d94bcda6bf3c6dfb05c38056a837ee
Ali <alighorbani20002@gmail.com>
2020-07-05 11:23:09 +0430

    add user dependency method and compact in the one method

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -4,10 +4,51 @@ namespace App\Repositories;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserRepository
 {
 
+    private $id;
+
+    public function setUserId($userId)
+    {
+        $this->id = $userId;
+    }
+
+    private function hasProject()
+    {
+        return DB::table('project_contractor')
+            ->where('contractor_id', $this->id)
+            ->exists();
+    }
+
+    private function isCreateProject()
+    {
+        return DB::table('projects')
+            ->where('project_creator', $this->id)
+            ->exists();
+    }
+
+    private function hasEarning()
+    {
+        return DB::table('earnings')
+            ->where('generator', $this->id)
+            ->exists();
+    }
+
+    private function hasCost()
+    {
+        return DB::table('costs')
+            ->where('generator', $this->id)
+            ->exists();
+    }
+
+    public function hasDependency($userId)
+    {
+        $this->setUserId($userId);
+        return ($this->hasProject() || $this->isCreateProject() || $this->hasEarning() || $this->hasCost());
+    }
 
     public function userUpdate(Request $request, User $user)
     {
