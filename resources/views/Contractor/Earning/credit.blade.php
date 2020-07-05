e5404d560269389779dc5e78e97f43b7012f9ad9
Ali <alighorbani20002@gmail.com>
2020-07-05 12:52:54 +0430

    resize and format credit view

--- resources/views/Contractor/Earning/credit.blade.php
+++ resources/views/Contractor/Earning/credit.blade.php
@@ -3,56 +3,56 @@
 @section('header', 'لیست بستانکاری های شما')
 @section('content')
 <div class="row">
-    <div class="@if(hasMember($credits)){{"col-sm-12"}}@else{{"col-sm-9 col-lg-offset-1"}}@endif">
-        <div class="card-box table-responsive">
+   <div class="col-sm-12">
+      <div class="card-box table-responsive">
+         @if(hasMember($credits))
+         <h4 class="header-title m-t-0 m-b-30 inb">
+            لیست بستانکاری های شما
+         </h4>
 
-            @if(hasMember($credits))
-            <h4 class="header-title m-t-0 m-b-30 inb">
-                لیست بستانکاری های شما
-            </h4>
+         <!-- Table Start !-->
+         <table id="datatable" class="table table-striped table-bordered">
+            <thead>
+               <tr>
+                  <th>ردیف</th>
+                  <th>عنوان بستانکاری</th>
+                  <th>ثبت کننده</th>
+                  <th class="tac">تاریخ ثبت</th>
+                  <th class="tac">زمان ثبت</th>
+                  <th>عنوان پروژه</th>
+                  <th>شناسه پروژه</th>
+                  <th>میزان درآمد (تومان)</th>
+                  <th class="tac">جزئیات</th>
+               </tr>
+            </thead>
+            <tbody>
+               @foreach ($credits as $row => $earning)
+               <tr>
+                  <td><?= $row  + 1 ?></td>
+                  <td>{{ $earning->title }}</td>
+                  <td>{{ $earning->name . " " . $earning->lastname  }}</td>
+                  <td class="tac date-show">{{ verta($earning->created_at)->formatJalaliDate() }}</td>
+                  <td class="tac date-show">{{ verta($earning->created_at)->format('h:m') }}</td>
+                  <td>{{ $earning->project_title }}</td>
+                  <td>{{ $earning->unique_id }}</td>
+                  <td>{{ number_format($earning->money_paid) }}</td>
+                  <td class="tac">
+                     <a href="{{ route('contractor.earning.show', $earning->id) }}"
+                        class="btn btn-icon waves-effect waves-light btn-danger m-b-5">
+                         <i class="fa fa-file-text-o"></i> 
+                    </a>
+                  </td>
+               </tr>
+               @endforeach
+            </tbody>
+         </table>
+         <!-- Table End !-->
 
-            <table id="datatable" class="table table-striped table-bordered">
-                <thead>
-                    <tr>
-                        <th>ردیف</th>
-                        <th>عنوان بستانکاری</th>
-                        <th>ثبت کننده</th>
-                        <th class="tac">تاریخ ثبت</th>
-                        <th class="tac">زمان ثبت</th>
-                        <th>عنوان پروژه</th>
-                        <th>شناسه پروژه</th>
-                        <th>میزان درآمد (تومان)</th>
-                        <th class="tac">جزئیات</th>
-                    </tr>
-                </thead>
-
-                <tbody>
-                    @foreach ($credits as $row => $earning)
-                    <tr>
-                        <td><?= $row  + 1 ?></td>
-                        <td>{{ $earning->title }}</td>
-                        <td>{{ $earning->name . " " . $earning->lastname  }}</td>
-                        <td class="tac date-show">{{ verta($earning->created_at)->formatJalaliDate() }}</td>
-                        <td class="tac date-show">{{ verta($earning->created_at)->format('h:m') }}</td>
-                        <td>{{ $earning->project_title }}</td>
-                        <td>{{ $earning->unique_id }}</td>
-                        <td>{{ number_format($earning->money_paid) }}</td>
-                        <td class="tac">
-                            <a href="{{ route('contractor.earning.show', $earning->id) }}"
-                                class="btn btn-icon waves-effect waves-light btn-danger m-b-5"> <i
-                                    class="fa fa-file-text-o"></i> </a>
-                        </td>
-                    </tr>
-                    @endforeach
-
-                </tbody>
-            </table>
-            @else
-            {!! recordMessage("بستانکاری برای شما ثبت نشده است.") !!}
-            @endif
-
-            {{ $credits->links() }}
-        </div>
-    </div>
+         @else
+         {!! recordMessage("بستانکاری برای شما ثبت نشده است.") !!}
+         @endif
+         {{ $credits->links() }}
+      </div>
+   </div>
 </div>
 @endsection
