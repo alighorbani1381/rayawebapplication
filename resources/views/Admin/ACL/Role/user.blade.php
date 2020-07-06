c65d5290f1f64d778df4f3c8ca91c48a8d55f615
Ali <alighorbani20002@gmail.com>
2020-07-06 20:55:13 +0430

    fixed user view

--- resources/views/Admin/ACL/Role/user.blade.php
+++ resources/views/Admin/ACL/Role/user.blade.php
@@ -7,38 +7,43 @@
         <div class="card-box">
             <h4 class="header-title m-t-0 m-b-30"></h4>
             <div class="row">
-                <div class="col-lg-11">
+                <div class="col-lg-10 col-lg-offset-1">
                     <form class="form-horizontal" role="form" action="{{ route('roles.user.store') }}" method="post">
                         @csrf
-
+                        <input type="hidden" name="user_id" value="{{ $user->id }}">
                         <div class="form-group">
-                            <h3>{{ $user->full_name }}</h3>
+                            
+                            @if($user->profile != 'default')
+                            <img style="width: 8%;display: inline-block;"
+                                src="{{ showPicture('admin.profile', $user->profile) }}" alt="{{ $user->fullName }}"
+                                title="{{ $user->fullName }}" class="img-circle img-thumbnail img-responsive">
+                            @else
+                            <img style="width: 8%;display: inline-block;" src="/admin/images/users/default.png" alt="{{ $user->fullName }}"
+                                title="{{ $user->fullName }}" class="img-circle img-thumbnail img-responsive">
+                            @endif
+
+                            <h3 style="display: inline-block;">{{ $user->full_name }}</h3>
                         </div>
 
                         <!-- roles Box !-->
                         <div class="form-group">
-                            <label style="height: 100%; margin-right:4%;">
+                            <label style="height: 100%;">
                                 نقش های این کاربر
                             </label>
 
-                            <div class="role-list">
+                            <select multiple class="form-control" name="role_id[]" required>
                                 @foreach($roles as $key => $role)
-                                <div class="pretty p-icon p-round p-pulse" style="margin: 8px;">
-                                    <input type="checkbox" name="role_id[]" value="{{$role->id}}">
-                                    <div class="state p-success">
-                                        <label>{{ $role->title}}</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                        <i class="icon mdi mdi-check"></i>
-                                    </div>
-                                </div>
+                                <option value="{{ $role->id }}">{{ $role->title }}</option>
                                 @endforeach
-                            </div>
+                            </select>
                         </div>
 
 
                         <!-- Submit Button !-->
                         <div class="form-group">
                             <button type="submit" class="btn btn-success waves-effect waves-light submit-button">
-                                افزودن کاربر
+                                ویرایش / افزودن 
+                                نقش ها
                             </button>
                         </div>
 
