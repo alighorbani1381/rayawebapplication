1590b0ce98b63e984ede2e7857b444f34f97bc22
Ali <alighorbani20002@gmail.com>
2020-07-07 19:18:11 +0430

    add comment to earning controller

--- app/Http/Controllers/Admin/EarningController.php
+++ app/Http/Controllers/Admin/EarningController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Project;
 use App\Earning;
 use Illuminate\Http\Request;
 use App\Request\EarningRequest;
@@ -26,16 +25,17 @@ class EarningController extends AdminController
         });
     }
 
+    # Show List of Earnings
     public function index()
     {
         $earnings = $this->repo->getEarningsList();
         return view('Admin.Earning.index', compact('earnings'));
     }
 
-
+    # Create Earning 
     public function create($earning = null)
     {
-        $projects = $this->repo->getProjectWant($earning);  
+        $projects = $this->repo->getProjectWant($earning);
 
         if ($projects->count() != 0)
             return view('Admin.Earning.create', compact('projects'));
@@ -44,7 +44,7 @@ class EarningController extends AdminController
         return redirect()->route('projects.create');
     }
 
-
+    # Store Earning
     public function store(Request $request)
     {
         EarningRequest::storeValidate($request);
@@ -52,21 +52,21 @@ class EarningController extends AdminController
         return redirect()->route('earnings.index');
     }
 
-
+    # Show Earning Detail
     public function show($earning)
     {
         $earning = $this->repo->getEarning($earning);
         return view('Admin.Earning.show', compact('earning'));
     }
 
-
+    # Edit Earnings
     public function edit(Earning $earning)
     {
         $projects  = $this->repo->getActiveProject();
         return view('Admin.Earning.edit', compact('projects', 'earning'));
     }
 
-
+    # Update Earning
     public function update(Request $request, Earning $earning)
     {
         $earning->update($request->all());
@@ -74,7 +74,7 @@ class EarningController extends AdminController
         return redirect()->route('earnings.show', $earning->id);
     }
 
-
+    # Remove Earning
     public function destroy(Earning $earning)
     {
         $earning->delete();
