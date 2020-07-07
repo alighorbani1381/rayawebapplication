bfe109d6132e90dfbbf16524783515e057f653b8
Ali <alighorbani20002@gmail.com>
2020-07-07 20:38:32 +0430

    add cost static controller

--- app/Http/Controllers/Admin/CoststaticController.php
+++ app/Http/Controllers/Admin/CoststaticController.php
@@ -13,23 +13,25 @@ class CoststaticController extends AdminController
   
     public function __construct()
     {
+        # Configuration of Repository
         $this->repo = resolve(CostStaticRepository::class);
     }
 
+    # Show Cost Static List
     public function index()
     {
         $staticCosts = $this->repo->getStaticCosts();
         return view('Admin.CostStatic.index', compact('staticCosts'));
     }
 
-
+    # Create Cost Static
     public function create()
     {
         $mainCategories = $this->repo->getMainCostStatic();
         return view('Admin.CostStatic.create', compact('mainCategories'));
     }
 
-
+    # Store Cost Static
     public function store(Request $request)
     {
         $request->validate(['title' => 'required', 'child' => 'required']);
@@ -37,13 +39,13 @@ class CoststaticController extends AdminController
         return redirect()->route('static.index');
     }
 
-
+    # Show Cost Static
     public function show()
     {
         return redirect()->route('static.index');
     }
 
-
+    # Edit Cost Static
     public function edit($costStatic)
     {
         $costStatic = $this->repo->getCostStatic($costStatic);
@@ -51,6 +53,7 @@ class CoststaticController extends AdminController
         return view('Admin.CostStatic.edit', compact('costStatic', 'mainCategories'));
     }
 
+    # Update Cost Static
     public function update(Request $request, $costStatic)
     {
         $costStatic = $this->repo->getCostStatic();
@@ -59,6 +62,7 @@ class CoststaticController extends AdminController
         return redirect()->route('static.index');
     }
 
+    # Remove Cost Static
     public function destroy($costStatic)
     {
         $costStatic = $this->repo->getCostStatic($costStatic);
