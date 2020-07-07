180394b992159617e5653f32c986f0f0fa328117
Ali <alighorbani20002@gmail.com>
2020-07-07 13:07:48 +0430

    add ACL Class to helper

--- app/helpers.php
+++ app/helpers.php
@@ -1,5 +1,41 @@
 <?php
 
+use Illuminate\Support\Facades\Gate;
+
+class ACL{
+
+    static function getUsers()
+    {
+        return ( Gate::allows('Index-User') || Gate::allows('Edit-User') || Gate::allows('Delete-User'));
+    }
+
+    static function getCostStatic()
+    {
+        return ( Gate::allows('Index-Cost-Static') || Gate::allows('Edit-Cost-Static') || Gate::allows('Delete-Cost-Static') || Gate::allows('Delete-Cost'));
+    }
+
+    static function getCosts()
+    {
+        return ( Gate::allows('Index-Cost') || Gate::allows('Show-Cost') || Gate::allows('Edit-Cost') || Gate::allows('Delete-Cost'));
+    }
+
+    static function getEarnings()
+    {
+        return ( Gate::allows('Index-Earning') || Gate::allows('Show-Earning') || Gate::allows('Edit-Earning') || Gate::allows('Delete-Earning'));
+    }
+
+    static function getProjects()
+    {
+        return  ( Gate::allows('Index-Project') || Gate::allows('Create-Earning-Project') || Gate::allows('Show-Project') || Gate::allows('Edit-Project') || Gate::allows('Delete-Project') );        
+    }
+
+    static function getCategories()
+    {
+        return ( Gate::allows('Index-Category') || Gate::allows('Edit-Category') || Gate::allows('Delete-Category'));
+    }
+
+}
+
 function hasMember($collection)
 {
     return is_countable($collection) && count($collection) != 0;
