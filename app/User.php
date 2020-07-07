7c68bfc12dd6f98618c832467291653c3dc6249f
Ali <alighorbani20002@gmail.com>
2020-07-07 13:08:04 +0430

    add check hasRole function

--- app/User.php
+++ app/User.php
@@ -63,4 +63,18 @@ class User extends Authenticatable
     {
         return $this->repo->hasDependency($this->id);
     }
+
+    public function hasRole($role)
+    {
+        if (is_string($role)) {
+            return $this->roles->contains('name', $role);
+        }
+
+        foreach ($role as $r) {
+            if ($this->hasRole($r->name)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
