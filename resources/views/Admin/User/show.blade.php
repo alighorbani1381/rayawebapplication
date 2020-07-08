fed0baf1aeed64f68191f0ccc1145a8ab6dc51d5
Ali <alighorbani20002@gmail.com>
2020-07-08 12:23:51 +0430

    show permission and role in user show

--- resources/views/Admin/User/show.blade.php
+++ resources/views/Admin/User/show.blade.php
@@ -3,7 +3,7 @@
 @section('header', 'جزئیات کاربر')
 @section('content')
 <div class="row">
-    <div class="@if($user->type == 'contractor'){{"col-md-8 col-md-offset-2"}}@else{{"col-md-5"}}@endif">
+    <div class="@if(!$user->isAdmin()){{"col-md-8 col-md-offset-2"}}@else{{"col-md-5"}}@endif">
         <div class="card-box task-detail">
             <div class="dropdown pull-right">
                 <a href="#" class="dropdown-toggle card-drop" data-toggle="dropdown" aria-expanded="false">
@@ -136,22 +136,34 @@
     </div>
 
 
-    @if($user->type == 'admin')
-    <div class="col-md-5 col-md-offset-1">
+    @if($user->isAdmin())
+    <div class="col-md-7">
         <div class="card-box task-detail">
             <h4 class="header-title m-t-0 m-b-30">
                 نقش ها و سطوح دسترسی
             </h4>
 
-            <div class="media m-b-20">
-                <div class="t">
-                    <h4 class="font-600"
-                        style="display: inline-block;margin-left:5px; font-weight:bold; color:rgb(0, 0, 59);">
-                        آدرس :
-                    </h4>
-                    <h4 class="font-600 m-b-20" style="display: inline-block;">{{ $user->address }}</h4>
-                </div>
-            </div>
+            <table id="datatable" class="tac-table table table-striped" style="border: 1px solid #dcdcdcdd;">
+                <tr class="tac">
+                    <td class="acl-list" style="background:#018c86;" colspan="4">نقش ها</td>
+                </tr>
+                @foreach($roles as $key => $role)
+                    @if(openTr($key, 4))<tr>@endif
+                        <td>{{ $role->title }}</td>
+                    @if(closeTr($key, 4))</tr>@endif
+                @endforeach
+            </table>
+
+            <table id="datatable" class="tac-table table table-striped" style="border: 1px solid #dcdcdcdd;">
+                <tr class="tac">
+                    <td class="acl-list" style="background:#6eaaf5;" colspan="4">سطوح دسترسی</td>
+                </tr>
+                @foreach($permissions as $key => $permission)
+                    @if(openTr($key, 4))<tr>@endif
+                        <td>{{ $permission }}</td>
+                    @if(closeTr($key, 4))</tr>@endif
+                @endforeach
+            </table>
             <div class="clearfix"></div>
         </div>
     </div>
