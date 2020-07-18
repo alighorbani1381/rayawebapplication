ef1f751923fa4b7c022aa5afda792e9ea2b5a063
Ali <alighorbani20002@gmail.com>
2020-07-18 17:14:38 +0430

    add get status color in helper

--- app/helpers.php
+++ app/helpers.php
@@ -3,44 +3,44 @@
 use Illuminate\Support\Facades\Gate;
 
 # ACL Check Class use to 
-class ACL{
+class ACL
+{
 
     # Check Users Access
     static function getUsers()
     {
-        return ( Gate::allows('Index-User') || Gate::allows('Edit-User') || Gate::allows('Delete-User'));
+        return (Gate::allows('Index-User') || Gate::allows('Edit-User') || Gate::allows('Delete-User'));
     }
 
     # Check CostStatic Access
     static function getCostStatic()
     {
-        return ( Gate::allows('Index-Cost-Static') || Gate::allows('Edit-Cost-Static') || Gate::allows('Delete-Cost-Static') || Gate::allows('Delete-Cost'));
+        return (Gate::allows('Index-Cost-Static') || Gate::allows('Edit-Cost-Static') || Gate::allows('Delete-Cost-Static') || Gate::allows('Delete-Cost'));
     }
 
     # Check Costs Access
     static function getCosts()
     {
-        return ( Gate::allows('Index-Cost') || Gate::allows('Show-Cost') || Gate::allows('Edit-Cost') || Gate::allows('Delete-Cost'));
+        return (Gate::allows('Index-Cost') || Gate::allows('Show-Cost') || Gate::allows('Edit-Cost') || Gate::allows('Delete-Cost'));
     }
 
     # Check Earnings Access
     static function getEarnings()
     {
-        return ( Gate::allows('Index-Earning') || Gate::allows('Show-Earning') || Gate::allows('Edit-Earning') || Gate::allows('Delete-Earning'));
+        return (Gate::allows('Index-Earning') || Gate::allows('Show-Earning') || Gate::allows('Edit-Earning') || Gate::allows('Delete-Earning'));
     }
 
     # Check Project Access
     static function getProjects()
     {
-        return  ( Gate::allows('Index-Project') || Gate::allows('Create-Earning-Project') || Gate::allows('Show-Project') || Gate::allows('Edit-Project') || Gate::allows('Delete-Project') );        
+        return (Gate::allows('Index-Project') || Gate::allows('Create-Earning-Project') || Gate::allows('Show-Project') || Gate::allows('Edit-Project') || Gate::allows('Delete-Project'));
     }
 
     # Check Categories Access
     static function getCategories()
     {
-        return ( Gate::allows('Index-Category') || Gate::allows('Edit-Category') || Gate::allows('Delete-Category'));
+        return (Gate::allows('Index-Category') || Gate::allows('Edit-Category') || Gate::allows('Delete-Category'));
     }
-
 }
 
 # Helper method to Check @reurn collection
@@ -53,7 +53,7 @@ function hasMember($collection)
 function recordMessage($message = 'موردی جهت نمایش یافت نشد.')
 {
     $part1 = '<div class="col-lg-offset-2"><img class="exists-record" src="' . asset('admin/images/symbols/cherry.png') . '" alt="record Not Found!"><div class="notfound-content"><div class="notfound-header">';
-    $part2 = '<h3>' . $message . '.</h3>';
+    $part2 = '<h3 class="exists-record-message">' . $message . '.</h3>';
     $part3 = '</div><div class="notfound-body"></div></div></div>';
     $finalMessage = $part1 . $part2 . $part3;
     return $finalMessage;
@@ -70,21 +70,21 @@ function showPicture($type, $name)
             return $path;
             break;
 
-            case 'admin.profile':
-                $path = asset('profiles/admins') . '/' . $name;
-                return $path;
+        case 'admin.profile':
+            $path = asset('profiles/admins') . '/' . $name;
+            return $path;
             break;
 
-            case 'contract.image':
-                $path = asset('admin/images/projects/contracts') . '/' . $name;
-                return $path;
+        case 'contract.image':
+            $path = asset('admin/images/projects/contracts') . '/' . $name;
+            return $path;
             break;
 
-            case 'meli.image':
-                $path = asset('admin/images/projects/meli_code') . '/' . $name;
-                return $path;
+        case 'meli.image':
+            $path = asset('admin/images/projects/meli_code') . '/' . $name;
+            return $path;
             break;
-            
+
         default:
             return asset("admin/images/users/default.png");
             break;
@@ -99,7 +99,16 @@ function openTr($key, $row)
 
 function closeTr($key, $row)
 {
-    $row*=2;
-    $key+1 % $row == 0;
+    $row *= 2;
+    $key + 1 % $row == 0;
 }
 
+function getStatusColor(int $percent)
+{
+    $color = "";
+    if ($percent < 25) $color = "danger";
+    if ($percent >= 25 && $percent < 50) $color = "warning";
+    if ($percent >= 50 && $percent < 75) $color = "info";
+    if ($percent >= 75 && $percent <= 100) $color = "success";
+    return $color;
+}
